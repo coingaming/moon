f60232a96508970d36e7ead8630b0b7499662c3a
Margus Pärt <marguspart@Marguss-MacBook-Pro.local>
2020-10-28 13:38:52 +0200

    save

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,6 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
+
+import socket from "./socket"
 import "phoenix_html"
--- lib/moon_web/channels/user_socket.ex
+++ lib/moon_web/channels/user_socket.ex
@@ -1,6 +1,18 @@
 defmodule MoonWeb.UserSocket do
   use Phoenix.Socket
 
+
+  # def connect(%{"token" => token}, socket, _connect_info) do
+  #   # max_age: 1209600 is equivalent to two weeks in seconds
+  #   case Phoenix.Token.verify(socket, "user socket", token, max_age: 1_209_600) do
+  #     {:ok, user_id} ->
+  #       {:ok, assign(socket, :user, user_id)}
+
+  #     {:error, reason} ->
+  #       :error
+  #   end
+  # end
+
   @impl true
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
@@ -8,4 +20,5 @@ defmodule MoonWeb.UserSocket do
 
   @impl true
   def id(_socket), do: nil
+
 end
