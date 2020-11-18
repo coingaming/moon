b68a6b1205b714b5c269a0c3d63df68086c7e875
Margus Pärt <marguspart@Marguss-MacBook-Pro.local>
2020-11-18 22:10:41 +0200

    save

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,16 +4,11 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE") || "zMBKDyfVAWJLhzEomz2JktQhqVEH9GAnnmytbouvPqFKwbQBgN80QJb8/NbmGHgw"
 
 config :moon, MoonWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "80"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
