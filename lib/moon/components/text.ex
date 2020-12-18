7ce99983e163efb79031e8875ebe620b06b7d916
Margus Pärt <marguspart@Marguss-MacBook-Pro.local>
2020-12-18 03:00:09 +0200

    save

--- lib/moon/components/text.ex
+++ lib/moon/components/text.ex
@@ -4,12 +4,14 @@ defmodule Moon.Components.Text do
   prop(size, :integer)
   prop(is_bold, :boolean)
   prop(color, :string)
+  prop(add_class, :string)
+  prop(style, :string)
 
   def render(assigns) do
     ~H"""
     {{ asset_import @socket, "js/components/text" }}
 
-    <div class="moon-text" data-size={{ @size }} data-is-bold={{ @is_bold && "true" }} style={{ get_style(color: @color) }}>
+    <div class={{ "moon-text #{@add_class}" }} data-size={{ @size }} data-is-bold={{ @is_bold && "true" }} style={{ "#{@style}; #{get_style(color: @color)}" }}>
       <slot />
     </div>
     """
