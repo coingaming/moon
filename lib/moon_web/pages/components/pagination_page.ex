5fdfbff483fc8fa7217536e6e520e4b79d7c13d0
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2021-03-24 22:22:58 +0000

    Add side section example

--- lib/moon_web/pages/components/pagination_page.ex
+++ lib/moon_web/pages/components/pagination_page.ex
@@ -11,9 +11,12 @@ defmodule MoonWeb.Pages.Components.PaginationPage do
       assign(socket,
         theme_name: params["theme_name"] || "sportsbet-dark",
         active_page: __MODULE__,
+        total_pages: 10,
         current_page_number: 6,
         size_page_number: 6,
-        total_pages: 10
+        section_page_number: 6,
+        section_per_page: 5,
+        section_total_entries: 48
       )
 
     {:ok, socket}
@@ -42,7 +45,7 @@ defmodule MoonWeb.Pages.Components.PaginationPage do
           />
         </template>
 
-        <template slot="code">
+    <template slot="code">
       <#CodePreview>
         alias Moon.Components.Pagination
 
@@ -62,7 +65,7 @@ defmodule MoonWeb.Pages.Components.PaginationPage do
           {:noreply, socket}
         end
       </#CodePreview>
-        </template>
+    </template>
 
         <template slot="state">@current_page_number = {{ @current_page_number }}</template>
       </ExampleAndCode>
@@ -106,19 +109,67 @@ defmodule MoonWeb.Pages.Components.PaginationPage do
           </Stack>
         </template>
 
-        <template slot="code">
-          <#CodePreview>
-            <Pagination size="xsmall" />
+    <template slot="code">
+      <#CodePreview>
+        <Pagination size="xsmall" />
 
-            <Pagination size="small" />
+        <Pagination size="small" />
 
-            <Pagination size="medium" />
+        <Pagination size="medium" />
 
-            <Pagination size="large" />
-          </#CodePreview>
-        </template>
+        <Pagination size="large" />
+      </#CodePreview>
+    </template>
       </ExampleAndCode>
 
+      <Heading size=24 class="mt-4" is_regular>Example with a side section</Heading>
+
+      <ExampleAndCode show_state={{ true }} layout="column">
+        <template slot="example">
+          <Stack>
+            <div class="flex flex-wrap items-center">
+              <div class="w-1/4 mb-4 text-sm">{{ side_text(@section_page_number, @section_per_page, @section_total_entries) }}</div>
+
+              <div class="w-3/4">
+                <Pagination
+                  current_page_number={{ @section_page_number }}
+                  total_pages={{ @total_pages }}
+                  size="small"
+                  on_change="change_section_page"
+                />
+              </div>
+            </div>
+          </Stack>
+        </template>
+
+    <template slot="code">
+      <#CodePreview>
+        <div class="flex flex-wrap items-center">
+          <div class="w-1/4 mb-4 text-sm">
+            {{ side_text(@page_number, @per_page, @total_entries) }}
+          </div>
+
+          <div class="w-3/4">
+            <Pagination
+              current_page_number={{ @page_number }}
+              total_pages={{ @total_pages }}
+              size="small"
+              on_change="change_current_page"
+            />
+          </div>
+        </div>
+
+        def side_text(page_number, per_page, total_entries) do
+          min_entry = (page_number - 1) * per_page + 1
+          max_entry = min(min_entry + per_page - 1, total_entries)
+
+          "Showing #{min_entry} – #{max_entry} of #{total_entries}"
+        end
+      </#CodePreview>
+    </template>
+
+        <template slot="state">@page_number = {{ @section_page_number }}<br>@per_page = {{ @section_per_page }}<br>@total_entries = {{ @section_total_entries }}</template>
+      </ExampleAndCode>
     </Stack>
     """
   end
@@ -131,5 +182,17 @@ defmodule MoonWeb.Pages.Components.PaginationPage do
   def handle_event("change_size_page", %{"page" => page}, socket) do
     socket = assign(socket, size_page_number: String.to_integer(page))
     {:noreply, socket}
+  end
+
+  def handle_event("change_section_page", %{"page" => page}, socket) do
+    socket = assign(socket, section_page_number: String.to_integer(page))
+    {:noreply, socket}
+  end
+
+  def side_text(page_number, per_page, total_entries) do
+    min_entry = (page_number - 1) * per_page + 1
+    max_entry = min(min_entry + per_page - 1, total_entries)
+
+    "Showing #{min_entry} – #{max_entry} of #{total_entries}"
   end
 end
