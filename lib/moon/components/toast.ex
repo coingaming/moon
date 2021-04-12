85b71d3247da44c809e39d5789d60c0627f8638b
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2021-04-12 23:21:52 +0100

    Rewrite Toast to a stateful component

--- lib/moon/components/toast.ex
+++ lib/moon/components/toast.ex
@@ -1,34 +1,54 @@
 defmodule Moon.Components.Toast do
-  use Moon.StatelessComponent
+  use Moon.StatefulComponent
+  alias Moon.Assets.Icons.IconBannerInfo
+  alias Moon.Assets.Icons.IconError
   alias Moon.Assets.Icons.IconErrorCircle
   alias Moon.Assets.Icons.IconSnackbarSuccess
+  alias Moon.Assets.Icons.IconWarning
 
-  prop message, :string
-  prop variant, :string, values: ~w(warning error info success)
-  prop closeable, :boolean, default: false
-  prop hidden, :boolean, default: true
-  prop on_show, :event
+  @default_duration 4000
+
+  prop message, :string, required: true
+  prop variant, :string, values: ~w(error warning info success)
+  prop closeable, :boolean, default: true
+
+  data show, :boolean, default: false
 
   def render(assigns) do
     ~H"""
     {{ asset_import @socket, "js/tailwind" }}
 
-    <div aria-live="assertive" class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end z-10">
-      <!--
-        Notification panel, dynamically insert this into the live region when it needs to be displayed
-
-        Entering: "transform ease-out duration-300 transition"
-          From: "translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-          To: "translate-y-0 opacity-100 sm:translate-x-0"
-        Leaving: "transition ease-in duration-100"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
-      <div class="w-full max-w-sm overflow-hidden bg-gohan-100 rounded-lg shadow-xl pointer-events-auto">
+    <div aria-live="assertive" class="fixed inset-0 z-10 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+      <div
+        class={{
+          "transform ease-in-out duration-300 transition w-full max-w-sm overflow-hidden bg-gohan-100 rounded-lg shadow-xl pointer-events-auto",
+          "opacity-100": @show,
+          "opacity-0 hidden": !@show
+        }}
+      >
         <div class="p-4">
           <div class="flex items-center">
             <div :if={{ @variant }} class="flex-shrink-0 mr-3">
+              <IconError
+                :if={{ @variant == "error" }}
+                class="block w-5 h-5"
+                font_size="1.25rem"
+              />
+
+              <IconWarning
+                :if={{ @variant == "warning" }}
+                class="block w-5 h-5"
+                font_size="1.25rem"
+              />
+
+              <IconBannerInfo
+                :if={{ @variant == "info" }}
+                class="block w-5 h-5"
+                font_size="1.25rem"
+              />
+
               <IconSnackbarSuccess
+                :if={{ @variant == "success" }}
                 class="block w-5 h-5"
                 font_size="1.25rem"
               />
@@ -40,8 +60,11 @@ defmodule Moon.Components.Toast do
               </p>
             </div>
 
-            <div class="flex flex-shrink-0 ml-4">
-              <button class="inline-flex rounded-md hover:text-gray-500 focus:outline-none">
+            <div :if={{ @closeable }} class="flex flex-shrink-0 ml-4">
+              <button
+                class="inline-flex rounded-md hover:text-gray-500 focus:outline-none"
+                :on-click="hide"
+              >
                 <span class="sr-only">Close</span>
                 <IconErrorCircle
                   class="w-5 h-5"
@@ -55,5 +78,15 @@ defmodule Moon.Components.Toast do
       </div>
     </div>
     """
+  end
+
+  def show(id, autohide \\ true, duration \\ @default_duration) do
+    send_update(__MODULE__, id: id, show: true)
+
+    if autohide, do: send_update_after(__MODULE__, [id: id, show: false], duration)
+  end
+
+  def handle_event("hide", _, socket) do
+    {:noreply, assign(socket, show: false)}
   end
 end
