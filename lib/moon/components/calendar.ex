ad4d7009acdd980fb93c6e35e7a5601ed1476565
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2021-05-12 18:35:24 +0100

    Create calendar page

---
+++ lib/moon/components/calendar.ex
@@ -0,0 +1,11 @@
+defmodule Moon.Components.Calendar do
+  use Moon.StatelessComponent
+
+  def render(assigns) do
+    ~H"""
+    <div class="">
+      Calendar Component
+    </div>
+    """
+  end
+end
---
+++ lib/moon_web/pages/components/calendar_page.ex
@@ -0,0 +1,48 @@
+defmodule MoonWeb.Pages.Components.CalendarPage do
+  use MoonWeb, :live_view
+  alias MoonWeb.Components.ExampleAndCode
+  alias Moon.Components.Calendar
+  alias Moon.Components.CodePreview
+  alias Moon.Components.Heading
+  alias Moon.Components.Link
+  alias Moon.Components.Stack
+
+  def mount(params, _session, socket) do
+    socket =
+      assign(socket,
+        theme_name: params["theme_name"] || "sportsbet-dark",
+        active_page: __MODULE__,
+        events: []
+      )
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <Stack>
+      <Heading size=32>Calendar</Heading>
+
+      <p>
+        <Link to="https://www.figma.com/file/S3q1SkVngbwHuwpxHKCsgtJj/Moon---Components?node-id=26127%3A3454">Figma design</Link>
+        <Link to="https://github.com/coingaming/moon/blob/master/lib/moon_web/pages/components/calendar_page.ex">Sourcecode of this page</Link>
+        <Link to="https://moon.io/toolkit/calendar">React implementation</Link>
+      </p>
+
+      <ExampleAndCode show_state={{ true }}>
+        <template slot="example">
+          <Calendar />
+        </template>
+
+        <template slot="code">
+      <#CodePreview>
+        <Calendar />
+      </#CodePreview>
+        </template>
+
+        <template slot="state">@events = {{ inspect(@events, pretty: true) }}</template>
+      </ExampleAndCode>
+    </Stack>
+    """
+  end
+end
