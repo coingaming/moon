ab8e79facc980896655aa66a90d506495bfaf54d
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2021-05-26 01:03:48 +0100

    Make sidebar expandable

--- lib/moon/components/sidebar.ex
+++ lib/moon/components/sidebar.ex
@@ -1,117 +1,81 @@
 defmodule Moon.Components.Sidebar do
   use Moon.StatelessComponent
+  alias Moon.Assets.Icons.IconChevronLeftRounded
+  alias Moon.Assets.Icons.IconChevronRightRounded
+  alias Moon.Assets.Icons.IconSlots
+  alias Moon.Assets.Icons.IconSportsBaseBall
+  alias Moon.Assets.Logos.LogoBitcasinoFull
+  alias Moon.Assets.Logos.LogoBitcasinoShort
 
   def render(assigns) do
     ~H"""
-    <div class="h-screen flex overflow-hidden bg-gray-100">
-      <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-      <div class="fixed inset-0 flex z-40 md:hidden" role="dialog" aria-modal="true">
-        <!--
-          Off-canvas menu overlay, show/hide based on off-canvas menu state.
+    <div
+      x-data="{ collapsed: false, showBtn: false }"
+      class="flex flex-shrink-0"
+    >
+      <div
+        x-cloak
+        x-show="collapsed"
+        class="p-5 flex items-center"
+      >
+        <!-- Short logo -->
+        <div class="flex items-center h-10">
+          <LogoBitcasinoShort font_size="2.1rem" />
+        </div>
 
-          Entering: "transition-opacity ease-linear duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-          Leaving: "transition-opacity ease-linear duration-300"
-            From: "opacity-100"
-            To: "opacity-0"
-        -->
-        <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+        <!-- Expand button -->
+        <div class="ml-2">
+          <button
+            type="button"
+            class="flex items-center justify-center h-6 w-6 rounded-full focus:outline-none shadow-lg"
+            x-cloak
+            @click="collapsed = false"
+          >
+            <span class="sr-only">Expand sidebar</span>
+            <IconChevronRightRounded font_size="0.75rem" />
+          </button>
+        </div>
+      </div>
 
-        <!--
-          Off-canvas menu, show/hide based on off-canvas menu state.
-
-          Entering: "transition ease-in-out duration-300 transform"
-            From: "-translate-x-full"
-            To: "translate-x-0"
-          Leaving: "transition ease-in-out duration-300 transform"
-            From: "translate-x-0"
-            To: "-translate-x-full"
-        -->
-        <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
-          <!--
-            Close button, show/hide based on off-canvas menu state.
-
-            Entering: "ease-in-out duration-300"
-              From: "opacity-0"
-              To: "opacity-100"
-            Leaving: "ease-in-out duration-300"
-              From: "opacity-100"
-              To: "opacity-0"
-          -->
-          <div class="absolute top-0 right-0 -mr-12 pt-2">
-            <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span class="sr-only">Close sidebar</span>
-              <!-- Heroicon name: outline/x -->
-              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+      <div
+        @mouseover="showBtn = true"
+        @mouseout="showBtn = false"
+        x-show="!collapsed"
+        class="flex flex-col w-56"
+      >
+        <div class="relative flex flex-col flex-grow p-5">
+          <!-- Collapse button -->
+          <div class="absolute top-0 right-0">
+            <button
+              type="button"
+              class="mt-7 -mr-3 flex items-center justify-center h-6 w-6 rounded-full focus:outline-none shadow-lg transition"
+              :class="{ 'opacity-100': showBtn, 'opacity-0': !showBtn }"
+              x-cloak
+              @click="collapsed = true"
+            >
+              <span class="sr-only">Collapse sidebar</span>
+              <IconChevronLeftRounded font_size="0.75rem" />
             </button>
           </div>
 
-          <div class="flex-shrink-0 flex items-center px-4">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow">
+          <!-- Full logo -->
+          <div class="flex items-center flex-shrink-0">
+            <LogoBitcasinoFull font_size="10rem" class="h-10" />
           </div>
-          <div class="mt-5 flex-1 h-0 overflow-y-auto">
-            <nav class="px-2 space-y-1">
-              <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-              <a href="#" class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                <!--
-                  Heroicon name: outline/home
 
-                  Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                -->
-                <svg class="text-gray-500 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
-                Dashboard
+          <div class="flex-grow">
+            <!-- Menu -->
+            <nav class="mt-6 space-y-3 h-96">
+              <a href="#" class="group flex items-center">
+                <IconSlots font_size="1.75rem" class="mr-3 flex-shrink-0 transition transform scale-100 group-hover:scale-110" />
+                <span class="group-hover:text-piccolo-100">Slots</span>
+              </a>
+              <a href="#" class="group flex items-center">
+                <IconSportsBaseBall font_size="1.75rem" class="mr-3 flex-shrink-0 transition transform scale-100 group-hover:scale-110" />
+                <span class="group-hover:text-piccolo-100">Baseball</span>
               </a>
             </nav>
           </div>
-        </div>
-
-        <div class="flex-shrink-0 w-14" aria-hidden="true">
-          <!-- Dummy element to force sidebar to shrink to fit close icon -->
-        </div>
-      </div>
-
-      <!-- Static sidebar for desktop -->
-      <div class="hidden md:flex md:flex-shrink-0">
-        <div class="flex flex-col w-64">
-          <!-- Sidebar component, swap this element with another sidebar if you like -->
-          <div class="flex flex-col flex-grow border-r border-gray-200 pt-5 pb-4 bg-white overflow-y-auto">
-            <div class="flex items-center flex-shrink-0 px-4">
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow">
-            </div>
-            <div class="mt-5 flex-grow flex flex-col">
-              <nav class="flex-1 px-2 bg-white space-y-1">
-                <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-                <a href="#" class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                  <!--
-                    Heroicon name: outline/home
-
-                    Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                  -->
-                  <svg class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                  </svg>
-                  Dashboard
-                </a>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="flex flex-col w-0 flex-1 overflow-hidden">
-        <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
-          <button class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
-            <span class="sr-only">Open sidebar</span>
-            <!-- Heroicon name: outline/menu-alt-2 -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
-            </svg>
-          </button>
         </div>
       </div>
     </div>
