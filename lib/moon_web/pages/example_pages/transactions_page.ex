8cd1e9f8e19e9caa61b8c94e08eed4c1abcab51e
Tankut Senturk <tankut.senturk@heathmont.net>
2021-06-30 17:21:46 +0300

    Refactoring transaction example initial commit

--- lib/moon_web/pages/example_pages/transactions_page.ex
+++ lib/moon_web/pages/example_pages/transactions_page.ex
@@ -1,16 +1,18 @@
 defmodule MoonWeb.Pages.ExamplePages.TransactionsPage do
   use MoonWeb, :live_view
 
+  alias Moon.Autolayouts.TopToDown
   alias Moon.Components.Heading
 
   alias MoonWeb.Pages.ExamplePages.Shared
   alias Shared.TopMenu
   alias Shared.LeftMenu
   alias Shared.Breadcrumbs
-
+  alias __MODULE__.TransactionsFilters
   alias __MODULE__.TransactionsList
 
-  data breadcrumbs, :any, default: [%{name: "Transactions", to: "/lab-light/example-pages/transactions"}]
+  data breadcrumbs, :any,
+    default: [%{name: "Transactions", to: "/lab-light/example-pages/transactions"}]
 
   def mount(params, _session, socket) do
     {:ok,
@@ -29,7 +31,10 @@ defmodule MoonWeb.Pages.ExamplePages.TransactionsPage do
         <div class="p-4 pt-8">
           <Breadcrumbs breadcrumbs={@breadcrumbs} />
           <Heading size={32} class="pt-4 pb-8">Transactions</Heading>
-          <TransactionsList id="transactions" />
+          <TopToDown>
+            <TransactionsFilters id="transaction_filters" />
+            <TransactionsList id="transactions" />
+          </TopToDown>
         </div>
       </div>
     </div>
