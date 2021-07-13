d9f2dcd17e6dd45dbd984a07c5bffeaddd7f76ca
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2021-07-13 23:58:43 +0100

    Tweak top menu

--- lib/moon_web/pages/example_pages/shared/top_menu.ex
+++ lib/moon_web/pages/example_pages/shared/top_menu.ex
@@ -10,24 +10,35 @@ defmodule MoonWeb.Pages.ExamplePages.Shared.TopMenu do
 
   def render(assigns) do
     ~F"""
-    <div class="flex gap-4 items-center bg-gohan-100 py-4 px-6">
+    <div class="flex items-center gap-4 px-6 py-4 bg-gohan-100">
       <div>
         <LogoLabFull font_size="3.75rem" class="block h-5" />
       </div>
 
       <Search id="search" />
 
-      <div class="flex gap-x-4 items-center">
-        <MenuButton class="text-trunks-100"><IconSettings /></MenuButton>
-        <MenuButton class="text-trunks-100"><IconLiveChat /></MenuButton>
-        <MenuButton class="text-trunks-100"><IconBell /></MenuButton>
-        <MenuButton class="text-gohan-100 bg-piccolo-100 text-xs relative rounded-sm">
+      <div class="flex items-center gap-x-4">
+        <MenuButton title="Settings">
+          <IconSettings />
+        </MenuButton>
+        <MenuButton title="Messages">
+          <IconLiveChat />
+        </MenuButton>
+        <MenuButton title="Notifications">
+          <IconBell />
+        </MenuButton>
+
+        <!-- TODO: Replace with avatar -->
+        <div
+          class="h-10 w-10 rounded flex items-center justify-center cursor-pointer relative text-sm text-gohan-100 bg-bulma-100"
+          title="Profile"
+        >
           <span>DN</span>
           <div
-            class="absolute right-0 bottom-0 w-3 h-3 border-2 rounded-full"
+            class="absolute bottom-0 right-0 w-3 h-3 border-2 rounded-full"
             style="background-color: #0CD664;"
           ></div>
-        </MenuButton>
+        </div>
       </div>
     </div>
     """
--- lib/moon_web/pages/example_pages/shared/top_menu/menu_button.ex
+++ lib/moon_web/pages/example_pages/shared/top_menu/menu_button.ex
@@ -3,11 +3,13 @@ defmodule MoonWeb.Pages.ExamplePages.Shared.TopMenu.MenuButton do
 
   slot default
   prop class, :string
+  prop title, :string
 
   def render(assigns) do
     ~F"""
     <div
-      class={"h-10 w-10 flex items-center justify-center cursor-pointer #{@class}"}
+      class={"h-10 w-10 rounded flex items-center justify-center cursor-pointer text-trunks-100 hover:text-bulma-100 hover:bg-goku-80 #{@class}"}
+      title={@title}
     >
       <#slot />
     </div>
