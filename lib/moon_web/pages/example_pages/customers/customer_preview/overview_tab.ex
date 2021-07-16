d8ba823b46fb497c8eedc8562751dd7faad15e17
Pavan Varma <pavan.varma@erlang-solutions.com>
2021-07-16 15:28:56 +0530

    Refactor row in overview tab into a seperate component

--- lib/moon_web/pages/example_pages/customers/customer_preview/overview_tab.ex
+++ lib/moon_web/pages/example_pages/customers/customer_preview/overview_tab.ex
@@ -1,10 +1,34 @@
+defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab.Row do
+  use MoonWeb, :stateless_component
+
+  slot col1, required: true
+  slot col2, required: true
+
+  def render(assigns) do
+    ~F"""
+    <div class="flex my-5">
+      <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
+        <#slot name="col1"/>
+      </div>
+      <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
+        <#slot name="col2"/>
+      </div>
+    </div>
+    """
+  end
+end
+
 defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab do
   use MoonWeb, :stateful_component
 
   alias Moon.Components.{Accordion, Divider, Button, Text}
   alias Moon.Autolayouts.LeftToRight
-
-  alias Moon.Assets.Icons.{IconUser, IconText, IconGlobe, IconMail, IconGeneralInfo, IconWalletRounded, IconPencil, IconAbout, IconSbShield, IconData, IconSettings, IconLightbulb, IconCalendarEmpty}
+  alias Moon.Assets.Icons.{
+    IconUser, IconText, IconGlobe, IconMail, IconGeneralInfo,
+    IconWalletRounded, IconPencil, IconAbout, IconSbShield,
+    IconData, IconSettings, IconLightbulb, IconCalendarEmpty
+  }
+  alias MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab.Row
 
   data show_account_details, :boolean, default: true
   data show_security, :boolean, default: true
@@ -18,171 +42,99 @@ defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab do
     <div>
       <Accordion>
         <Accordion.Item title="Account Details" click="toggle_account_details" is_open={@show_account_details}>
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconUser font_size="1rem"/> &nbsp;&nbsp;Name
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {@customer.name} &nbsp;&nbsp;({@customer.username})
-            </div>
-          </div>
+          <Row>
+            <:col1><IconUser font_size="1rem"/> &nbsp;&nbsp;Name</:col1>
+            <:col2>{@customer.name} &nbsp;&nbsp;({@customer.username})</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconUser font_size="1rem"/> &nbsp;&nbsp;Usr Scr ID
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {"#{@customer.id}"}
-            </div>
-          </div>
+          <Row>
+            <:col1><IconUser font_size="1rem"/> &nbsp;&nbsp;Usr Scr ID</:col1>
+            <:col2>{"#{@customer.id}"}</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconText font_size="1rem"/> &nbsp;&nbsp;Registration
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              3 months ago &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconText font_size="1rem"/> &nbsp;&nbsp;Registration</:col1>
+            <:col2>3 months ago &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconGlobe font_size="1rem"/> &nbsp;&nbsp;Reg. country
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {@customer.country} &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconGlobe font_size="1rem"/> &nbsp;&nbsp;Reg. country</:col1>
+            <:col2>{@customer.country} &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconCalendarEmpty font_size="1rem"/> &nbsp;&nbsp;Birthday
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {@customer.signup_at |> Timex.format!("%b %d, %Y", :strftime)}
-            </div>
-          </div>
+          <Row>
+            <:col1><IconCalendarEmpty font_size="1rem"/> &nbsp;&nbsp;Birthday</:col1>
+            <:col2>{@customer.signup_at |> Timex.format!("%b %d, %Y", :strftime)}</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconMail font_size="1rem"/> &nbsp;&nbsp;Email
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {@customer.email} &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconMail font_size="1rem"/> &nbsp;&nbsp;Email</:col1>
+            <:col2>{@customer.email} &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconGeneralInfo font_size="1rem"/> &nbsp;&nbsp;KYC
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              PENDING &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconGeneralInfo font_size="1rem"/> &nbsp;&nbsp;KYC</:col1>
+            <:col2>PENDING &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconUser font_size="1rem"/> &nbsp;&nbsp;Account Type
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              Player
-            </div>
-          </div>
+          <Row>
+            <:col1><IconUser font_size="1rem"/> &nbsp;&nbsp;Account Type</:col1>
+            <:col2>Player</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSettings font_size="1rem"/> &nbsp;&nbsp;Preffered Language
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              English
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSettings font_size="1rem"/> &nbsp;&nbsp;Preffered Language</:col1>
+            <:col2>English</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSettings font_size="1rem"/> &nbsp;&nbsp;Preffered Currency
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              EUR
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSettings font_size="1rem"/> &nbsp;&nbsp;Preffered Currency</:col1>
+            <:col2>  EUR</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconLightbulb font_size="1rem"/> &nbsp;&nbsp;Source
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              -
-            </div>
-          </div>
+          <Row>
+            <:col1><IconLightbulb font_size="1rem"/> &nbsp;&nbsp;Source</:col1>
+            <:col2>    -</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconData font_size="1rem"/> &nbsp;&nbsp;Value90
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              HV90
-            </div>
-          </div>
+          <Row>
+            <:col1><IconData font_size="1rem"/> &nbsp;&nbsp;Value90</:col1>
+            <:col2> HV90</:col2>
+          </Row>
         </Accordion.Item>
 
         <Divider class="mb-2"/>
 
         <Accordion.Item title="Security" click="toggle_security" is_open={@show_security}>
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;Chat Visits (last 30 days)
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              23
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;Chat Visits (last 30 days)</:col1>
+            <:col2>   23</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;Password changed in 48h
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              3h &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;Password changed in 48h</:col1>
+            <:col2>3h &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;Login Status
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              Login closed &nbsp;&nbsp; <IconPencil font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;Login Status</:col1>
+            <:col2>Login closed &nbsp;&nbsp; <IconPencil font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;Active sessions
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              3 &nbsp;&nbsp;
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;Active sessions</:col1>
+            <:col2>3 &nbsp;&nbsp;</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;First deposite made in 48h
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              22h &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;First deposite made in 48h</:col1>
+            <:col2>22h &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconSbShield font_size="1rem"/> &nbsp;&nbsp;Cashier
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              Non-Depositor &nbsp;&nbsp; <IconAbout font_size="1rem"/>
-            </div>
-          </div>
+          <Row>
+            <:col1><IconSbShield font_size="1rem"/> &nbsp;&nbsp;Cashier</:col1>
+            <:col2>Non-Depositor &nbsp;&nbsp; <IconAbout font_size="1rem"/></:col2>
+          </Row>
         </Accordion.Item>
 
         <Divider class="mb-2"/>
@@ -211,41 +163,25 @@ defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab do
             {/for}
           </LeftToRight>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Real Money Wallet
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              {get_money(@wallets_active_tab)}
-            </div>
-          </div>
+          <Row>
+            <:col1><IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Real Money Wallet</:col1>
+            <:col2>{get_money(@wallets_active_tab)}</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Locked Wallet
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              0
-            </div>
-          </div>
+          <Row>
+            <:col1><IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Locked Wallet</:col1>
+            <:col2>0</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Reward Wallet
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              0
-            </div>
-          </div>
+          <Row>
+            <:col1><IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Reward Wallet</:col1>
+            <:col2>0</:col2>
+          </Row>
 
-          <div class="flex my-5">
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              <IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Capped Wallet
-            </div>
-            <div class="w-1/2 flex-none flex align-start items-center text-md text-gohan-40">
-              0
-            </div>
-          </div>
+          <Row>
+            <:col1><IconWalletRounded font_size="1rem"/> &nbsp;&nbsp;Capped Wallet</:col1>
+            <:col2>0</:col2>
+          </Row>
         </Accordion.Item>
       </Accordion>
     </div>
