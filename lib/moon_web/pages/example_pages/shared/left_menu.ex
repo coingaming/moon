cc958e0e977d93ce44c532235f662f2f75721124
Pavan Varma <pavan.varma@erlang-solutions.com>
2021-08-05 13:07:33 +0530

    Show created segments in left navbar

--- lib/moon_web/pages/example_pages/shared/left_menu.ex
+++ lib/moon_web/pages/example_pages/shared/left_menu.ex
@@ -1,49 +1,93 @@
 defmodule MoonWeb.Pages.ExamplePages.Shared.LeftMenu do
-  use MoonWeb, :stateful_component
+  use MoonWeb, :stateless_component
+
+  alias Surface.Components.LiveRedirect
 
   alias Moon.Assets.Icon
   alias Moon.Autolayouts.Sections
   alias Moon.Components.Divider
 
-  data segments, :any
-
-  def mount(assigns) do
-    {:ok, assign(assigns, segments: get_segments())}
-  end
+  alias MoonWeb.MockDB.{Users, Segments, Utils}
 
   def render(assigns) do
+    segments = get_segments()
+
     ~F"""
     <Sections class="py-6 text-sm w-60">
-      <div :for={segment <- @segments}>
-        <div class="px-4 py-2 mx-2 mb-2 text-trunks-100">
-          {segment.title} • {segment.count}
-        </div>
-
-        <div class="space-y-0.5 m-2">
-          <div class="flex items-center px-2 cursor-pointer" :for={item <- segment.items}>
-            <div class="flex items-center justify-center w-6 h-6 m-2">
-              <Icon name={item.icon} class="text-trunks-100" />
-            </div>
-            <div class="">{item.title}</div>
-          </div>
-        </div>
-
-        <div class="m-2 mb-0" :if={segment.new_item}>
-          <div class="flex items-center px-2 cursor-pointer">
-            <div class="flex items-center justify-center w-6 h-6 m-2 rounded-sm bg-goku-80">
-              <Icon name="icon_plus" />
-            </div>
-            <div class="">{segment.new_item}</div>
+      <div :for={segment <- segments}>
+        {#if segment.title == "Segments"}
+          <div class="px-4 py-2 mx-2 mb-2 text-trunks-100">
+            {segment.title} • {segment.count}
           </div>
 
-          <Divider color="beerus-100" class="m-4 mb-0" />
-        </div>
+          <div class="space-y-0.5 m-2">
+            {#for item <- segment.items}
+              <LiveRedirect :if={item.type == :customers} to={"/example-pages/customers?segment_id=#{item.id}"}>
+                <div class="flex items-center px-2 cursor-pointer">
+                  <div class="flex items-center justify-center w-6 h-6 m-2">
+                    <Icon name={:icon_user} class="text-trunks-100" />
+                  </div>
+                  <div class="">{item.name}</div>
+                </div>
+              </LiveRedirect>
+
+              <LiveRedirect :if={item.type == :affiliates} to={"/example-pages/affiliates?segment_id=#{item.id}"}>
+                <div class="flex items-center px-2 cursor-pointer">
+                  <div class="flex items-center justify-center w-6 h-6 m-2">
+                    <Icon name={:icon_expand} class="text-trunks-100" />
+                  </div>
+                  <div class="">{item.name}</div>
+                </div>
+              </LiveRedirect>
+            {/for}
+
+          </div>
+
+          <div class="m-2 mb-0" :if={segment.new_item}>
+            <div class="flex items-center px-2 cursor-pointer">
+              <div class="flex items-center justify-center w-6 h-6 m-2 rounded-sm bg-goku-80">
+                <Icon name="icon_plus" />
+              </div>
+              <div class="">{segment.new_item}</div>
+            </div>
+
+            <Divider color="beerus-100" class="m-4 mb-0" />
+          </div>
+
+        {#else}
+          <div class="px-4 py-2 mx-2 mb-2 text-trunks-100">
+            {segment.title} • {segment.count}
+          </div>
+
+          <div class="space-y-0.5 m-2">
+            <div class="flex items-center px-2 cursor-pointer" :for={item <- segment.items}>
+              <div class="flex items-center justify-center w-6 h-6 m-2">
+                <Icon name={item.icon} class="text-trunks-100" />
+              </div>
+              <div class="">{item.title}</div>
+            </div>
+          </div>
+
+          <div class="m-2 mb-0" :if={segment.new_item}>
+            <div class="flex items-center px-2 cursor-pointer">
+              <div class="flex items-center justify-center w-6 h-6 m-2 rounded-sm bg-goku-80">
+                <Icon name="icon_plus" />
+              </div>
+              <div class="">{segment.new_item}</div>
+            </div>
+
+            <Divider color="beerus-100" class="m-4 mb-0" />
+          </div>
+        {/if}
       </div>
     </Sections>
     """
   end
 
   def get_segments() do
+    segments = Segments.list_all()
+      |> Utils.take_page(0, 6)
+
     [
       %{
         title: "Dashboards",
@@ -59,15 +103,9 @@ defmodule MoonWeb.Pages.ExamplePages.Shared.LeftMenu do
       },
       %{
         title: "Segments",
-        count: 5,
+        count: length(segments),
         new_item: "New segment",
-        items: [
-          %{icon: :icon_expand, title: "Yggdrasil games"},
-          %{icon: :icon_expand, title: "Rage clicked recently"},
-          %{icon: :icon_user, title: "New last week"},
-          %{icon: :icon_user, title: "Players report"},
-          %{icon: :icon_expand, title: "May bet count"}
-        ]
+        items: segments
       },
       %{
         title: "Following",
