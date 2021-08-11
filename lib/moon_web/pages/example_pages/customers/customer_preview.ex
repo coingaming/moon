277331da5ccfc52836e209ffb09c7630ac20af68
Pavan Varma <pavan.varma.msp@gmail.com>
2021-08-11 21:52:09 +0530

    Implement SlideOver component

--- lib/moon_web/pages/example_pages/customers/customer_preview.ex
+++ lib/moon_web/pages/example_pages/customers/customer_preview.ex
@@ -1,12 +1,11 @@
 defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview do
   use MoonWeb, :stateful_component
 
-  alias Moon.Components.{Text, Divider, Button}
+  alias Moon.Components.{Text, Divider, Button, SlideOver}
   alias Moon.Autolayouts.{LeftToRight}
-  alias Moon.Assets.Icons.{IconUser, IconShare, IconCloseRounded}
+  alias Moon.Assets.Icons.{IconUser, IconShare}
 
-  alias MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.OverviewTab
-  alias MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.RewardsTab
+  alias MoonWeb.Pages.ExamplePages.Customers.CustomerPreview.{OverviewTab, RewardsTab}
 
   data active_tab, :string, default: "overview"
 
@@ -15,75 +14,61 @@ defmodule MoonWeb.Pages.ExamplePages.Customers.CustomerPreview do
 
   def render(assigns) do
     ~F"""
-    <div class="fixed top-0 right-0 bottom-0 w-1/2 flex flex-col px-6 z-20 bg-gohan-100 shadow overflow-y-auto">
-      <div class="flex-none flex justify-between py-5 ">
-        <Text size="24" class="block">Quick View</Text>
-        <div class="flex items-center">
-          <IconUser font_size="1.5rem" class="mr-4" />
-          <IconShare font_size="1.5rem" />
-          <Divider orientation="vertical" class="mx-4"/>
-          <div class="p-2 rounded bg-goku-100 cursor-pointer" :on-click={@on_close}>
-            <IconCloseRounded font_size="1.5rem" />
-          </div>
-        </div>
-      </div>
+    <SlideOver on_close={@on_close}>
+      <:header><Text size="24" class="block">Quick View</Text></:header>
+      <:options>
+        <IconUser font_size="1.5rem" class="mr-4" />
+        <IconShare font_size="1.5rem" />
+        <Divider orientation="vertical" class="mx-4"/>
+      </:options>
 
-      <LeftToRight class="mb-4">
-        {#for tab <- ["overview", "rewards", "KPI", "cashier", "TXN", "comms", "fraud", "notes"]}
-          <Button
-            :if={tab == @active_tab}
-            on_click={"select_tab:#{tab}"}
-            size="xsmall"
-            class="rounded bg-goku-100"
-          >
-            <Text size="14" class="capitalize text-gohan-80">{tab}</Text>
-          </Button>
+      <:content>
+        <LeftToRight class="mb-4">
+          {#for tab <- ["overview", "rewards", "KPI", "cashier", "TXN", "comms", "fraud", "notes"]}
+            <Button
+              size="xsmall"
+              on_click={"select_tab:#{tab}"}
+              class={"rounded #{if tab == @active_tab, do: "bg-goku-100"}"}
+            >
+              <Text size="14" class="capitalize text-gohan-80">{tab}</Text>
+            </Button>
+          {/for}
+        </LeftToRight>
 
-          <Button
-            :if={tab != @active_tab}
-            on_click={"select_tab:#{tab}"}
-            variant="danger"
-            size="xsmall"
-            class="rounded"
-          >
-            <Text size="14" class="capitalize text-hit-80">{tab}</Text>
-          </Button>
-        {/for}
-      </LeftToRight>
-
-      {#case @active_tab}
-        {#match "overview"}
-          <OverviewTab id="overview_tab" customer={@customer} />
-        {#match "rewards"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <RewardsTab id="rewards_tab" />
-          </div>
-        {#match "KPI"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">TODO</Text>
-          </div>
-        {#match "cashier"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">TODO</Text>
-          </div>
-        {#match "TXN"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">TODO</Text>
-          </div>
-        {#match "comms"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">TODO</Text>
-          </div>
-        {#match "fraud"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">TODO</Text>
-          </div>
-        {#match "notes"}
-          <div class="flex items-center justify-around h-64 w-full">
-            <Text size="16">No notes found</Text>
-          </div>
-      {/case}
-    </div>
+        {#case @active_tab}
+          {#match "overview"}
+            <OverviewTab id="overview_tab" customer={@customer} />
+          {#match "rewards"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <RewardsTab id="rewards_tab" />
+            </div>
+          {#match "KPI"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">TODO</Text>
+            </div>
+          {#match "cashier"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">TODO</Text>
+            </div>
+          {#match "TXN"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">TODO</Text>
+            </div>
+          {#match "comms"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">TODO</Text>
+            </div>
+          {#match "fraud"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">TODO</Text>
+            </div>
+          {#match "notes"}
+            <div class="flex items-center justify-around h-64 w-full">
+              <Text size="16">No notes found</Text>
+            </div>
+        {/case}
+      </:content>
+    </SlideOver>
     """
   end
 
