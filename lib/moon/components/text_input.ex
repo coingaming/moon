f35ebb9d8458ede1d4332f750ae023f2568cde98
Margus Pää-äär <53759446+margusp2heathmont@users.noreply.github.com>
2021-08-12 12:47:42 +0300

    ran formatters

--- lib/moon/components/text_input.ex
+++ lib/moon/components/text_input.ex
@@ -42,27 +42,35 @@ defmodule Moon.Components.TextInput do
   def render(assigns) do
     ~F"""
     <div class="relative">
-      {asset_import @socket, "js/components/text-input"}
+      {asset_import(@socket, "js/components/text-input")}
 
       <Icon name={@left_icon} :if={@left_icon} class="absolute z-10 top-3 left-5" />
-      <Icon name={@right_icon} click={@right_icon_click} :if={@right_icon} class="absolute z-10 top-3 right-5" />
+      <Icon
+        name={@right_icon}
+        click={@right_icon_click}
+        :if={@right_icon}
+        class="absolute z-10 top-3 right-5"
+      />
 
       <TextInput
-        class={"w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
-        border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
-        placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
-        disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
-        z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
-        "pl-12": @left_icon, "pr-12": @right_icon,"data-rounded": @rounded, "border-chi-chi-100": @error}
-        field={@field}
-        opts={
-          [
-            placeholder: @placeholder,
-            disabled: @disabled,
-            "data-error": @error && "true",
-            "data-rounded": @rounded && "true"
-          ]
+        class={
+          "w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
+                border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
+                placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
+                disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
+                z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
+          "pl-12": @left_icon,
+          "pr-12": @right_icon,
+          "data-rounded": @rounded,
+          "border-chi-chi-100": @error
         }
+        field={@field}
+        opts={[
+          placeholder: @placeholder,
+          disabled: @disabled,
+          "data-error": @error && "true",
+          "data-rounded": @rounded && "true"
+        ]}
         value={@value}
         focus={@on_focus}
         blur={@on_blur}
@@ -71,13 +79,17 @@ defmodule Moon.Components.TextInput do
 
       <Label text={@label} :if={@label}>
         <TextInput
-        class={"w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
-        border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
-        placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
-        disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
-        z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
-        "pl-12": @left_icon, "pr-12": @right_icon,"data-rounded": @rounded, "data-error": @error}
-
+          class={
+            "w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
+                  border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
+                  placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
+                  disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
+                  z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
+            "pl-12": @left_icon,
+            "pr-12": @right_icon,
+            "data-rounded": @rounded,
+            "data-error": @error
+          }
           field={@field}
           opts={[
             placeholder: @placeholder,
