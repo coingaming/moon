6b6364ef0e0e2a792148586da2b420a8712953cb
Margus Pää-äär <53759446+margusp2heathmont@users.noreply.github.com>
2021-08-12 13:04:16 +0300

    redid text input a bit

--- lib/moon/components/text_input.ex
+++ lib/moon/components/text_input.ex
@@ -1,7 +1,71 @@
-defmodule Moon.Components.TextInput do
+defmodule Moon.Components.TextInput.TextInputInternal do
   use Moon.StatelessComponent
 
   alias Surface.Components.Form.TextInput
+
+  prop(field, :atom)
+
+  prop(type, :string,
+    default: "text",
+    values: [
+      "date",
+      "datetime-local",
+      "email",
+      "number",
+      "password",
+      "search",
+      "tel",
+      "text",
+      "url",
+      "time",
+      "url"
+    ]
+  )
+
+  prop(value, :string)
+  prop(placeholder, :string)
+  prop(error, :string)
+  prop(rounded, :boolean)
+  prop(disabled, :boolean)
+  prop(required, :boolean)
+  prop(left_icon, :string)
+  prop(right_icon, :string)
+  prop(right_icon_click, :event)
+  prop(class, :string)
+  prop(without_design, :boolean)
+  prop(on_focus, :event)
+  prop(on_blur, :event)
+
+  def render(assigns) do
+    ~F"""
+    <TextInput
+      class={
+        "w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
+                              border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
+                              placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
+                              disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
+                              z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
+        "pl-12": @left_icon,
+        "pr-12": @right_icon,
+        "border-chi-chi-100": @error
+      }
+      field={@field}
+      opts={[
+        placeholder: @placeholder,
+        disabled: @disabled
+      ]}
+      value={@value}
+      focus={@on_focus}
+      blur={@on_blur}
+    />
+    """
+  end
+end
+
+defmodule Moon.Components.TextInput do
+  use Moon.StatelessComponent
+
+  alias __MODULE__.TextInputInternal
   alias Moon.Components.Label
   alias Moon.Assets.Icon
 
@@ -52,54 +116,31 @@ defmodule Moon.Components.TextInput do
         class="absolute z-10 top-3 right-5"
       />
 
-      <TextInput
-        class={
-          "w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
-                border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
-                placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
-                disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
-                z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
-          "pl-12": @left_icon,
-          "pr-12": @right_icon,
-          "data-rounded": @rounded,
-          "border-chi-chi-100": @error
-        }
-        field={@field}
-        opts={[
-          placeholder: @placeholder,
-          disabled: @disabled,
-          "data-error": @error && "true",
-          "data-rounded": @rounded && "true"
-        ]}
-        value={@value}
-        focus={@on_focus}
-        blur={@on_blur}
-        :if={!@label}
+      <TextInputInternal
+        {=@class}
+        {=@field}
+        {=@left_icon}
+        {=@right_icon}
+        {=@error}
+        {=@placeholder}
+        {=@disabled}
+        {=@value}
+        {=@on_focus}
+        {=@on_blur}
       />
 
       <Label text={@label} :if={@label}>
-        <TextInput
-          class={
-            "w-full max-w-full px-4 py-2 mt-2 bg-gohan-100 appearance-none text-base leading-normal text-color-bulma-100
-                  border border-solid border-beerus-100 rounded transition ease-in placeholder-trunks-100
-                  placeholder-opacity-100 hover:boder hover:border-goku-40 hover:focus:border-none
-                  disabled:opacity-50 disabled:cursor-not-allowed focus:border-piccolo-100 focus:outline-none
-                  z-0 relative no-scrollbar invalid:shadow-none invalid:border-chi-chi-100",
-            "pl-12": @left_icon,
-            "pr-12": @right_icon,
-            "data-rounded": @rounded,
-            "data-error": @error
-          }
-          field={@field}
-          opts={[
-            placeholder: @placeholder,
-            disabled: @disabled,
-            "data-error": @error && "true",
-            "data-rounded": @rounded && "true"
-          ]}
-          focus={@on_focus}
-          blur={@on_blur}
-          value={@value}
+        <TextInputInternal
+          {=@class}
+          {=@field}
+          {=@left_icon}
+          {=@right_icon}
+          {=@error}
+          {=@placeholder}
+          {=@disabled}
+          {=@value}
+          {=@on_focus}
+          {=@on_blur}
         />
       </Label>
     </div>
