5225ffaa52f549e35c3e134984d0a0f8ecd6e408
Anil Wadghule <anildigital@gmail.com>
2021-08-18 23:18:28 +0530

    Enable Tailwind JIT mode

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,6 +2,7 @@ const plugin = require('tailwindcss/plugin')
 const invalidPlugin = require('./tailwind/variants/invalid')
 
 module.exports = {
+  mode: "jit",
   purge: [
     '../lib/**/*.ex',
     '../lib/**/*.leex',
