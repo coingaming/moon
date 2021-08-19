f0e3dd474b64babc27828cfefa3c4becca6955bb
Anil Wadghule <anildigital@gmail.com>
2021-08-20 01:16:22 +0530

    Style heading component with tailwind css; Update examples

--- lib/moon/components/heading.ex
+++ lib/moon/components/heading.ex
@@ -14,9 +14,23 @@ defmodule Moon.Components.Heading do
 
     <div
       id={@id}
-      class={"moon-heading #{@class}"}
-      data-size={@size}
-      data-is-regular={@is_regular && "true"}
+      class={
+        "text-bulma-100 #{@class}",
+        "text-xs leading-4": @size == 10,
+        "text-sm leading-4": @size == 12,
+        "text-sm leading-5": @size == 14,
+        "text-base leading-6": @size == 16,
+        "text-lg leading-7": @size == 18,
+        "text-xl leading-7": @size == 20,
+        "text-2xl leading-8": @size == 24,
+        "text-3xl leading-10": @size == 32,
+        "text-4xl leading-10": @size == 48,
+        "text-5xl leading-10": @size == 56,
+        "text-6xl leading-10": @size == 64,
+        "text-7xl leading-10": @size == 72,
+        "font-semibold": !@is_regular,
+        "font-normal": @is_regular == true
+      }
       style={get_style(color: @color)}
     >
       <#slot />
