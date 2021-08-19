17d9e8008825dbb3086b865883a3f7d981a5b14e
Anil Wadghule <anildigital@gmail.com>
2021-08-20 00:58:09 +0530

    Style text component with tailwind css; Update examples

--- lib/moon/components/text.ex
+++ lib/moon/components/text.ex
@@ -13,9 +13,22 @@ defmodule Moon.Components.Text do
     {asset_import(@socket, "js/components/text")}
 
     <div
-      class={"moon-text #{@class}"}
-      data-size={@size}
-      data-is-bold={@is_bold && "true"}
+      class={
+        "#{@class}",
+        "text-xs leading-4": @size == 10,
+        "text-sm leading-4": @size == 12,
+        "text-sm leading-5": @size == 14,
+        "text-base leading-6": @size == 16,
+        "text-lg leading-7": @size == 18,
+        "text-xl leading-7": @size == 20,
+        "text-2xl leading-8": @size == 24,
+        "text-3xl leading-10": @size == 32,
+        "text-4xl leading-10": @size == 48,
+        "text-5xl leading-10": @size == 56,
+        "text-6xl leading-10": @size == 64,
+        "text-7xl leading-10": @size == 72,
+        "font-semibold": @is_bold == true
+      }
       style={get_style(@style, color: @color)}
     >
       <#slot />
