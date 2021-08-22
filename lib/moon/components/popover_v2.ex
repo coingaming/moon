1c9db772a10128f3eca23a2fc292b79e966c03f4
Pavan Varma <pavan.varma.msp@gmail.com>
2021-08-22 14:29:40 +0530

    Fix close on outside issue

--- lib/moon/components/popover_v2.ex
+++ lib/moon/components/popover_v2.ex
@@ -35,61 +35,61 @@ defmodule Moon.Components.PopoverV2 do
 
       {#case @placement}
         {#match "top-start"}
-          <div :if={@show} class="absolute top-0 left-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute top-0 left-0 z-40">
             <div class="transform -translate-y-full pb-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "top"}
-          <div :if={@show} class="absolute left-0 top-0 right-0 flex flex-col items-center z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 top-0 right-0 flex flex-col items-center z-40">
             <div class="transform -translate-y-full pb-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "top-end"}
-          <div :if={@show} class="absolute top-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute top-0 right-0 z-40">
             <div class="transform -translate-y-full pb-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "right-start"}
-          <div :if={@show} class="absolute left-0 top-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 top-0 right-0 z-40">
             <div class="transform translate-x-full pl-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "right"}
-          <div :if={@show} class="absolute left-0 top-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 top-0 right-0 z-40">
             <div class="transform translate-x-full -translate-y-1/3 pl-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "right-end"}
-          <div :if={@show} class="absolute left-0 bottom-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 bottom-0 right-0 z-40">
             <div class="transform translate-x-full pl-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "bottom-start"}
-          <div :if={@show} class="absolute left-0 bottom-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 bottom-0 z-40">
             <div class="transform translate-y-full pt-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "bottom"}
-          <div :if={@show} class="absolute left-0 bottom-0 right-0 flex flex-col items-center z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 bottom-0 right-0 flex flex-col items-center z-40">
             <div class="transform translate-y-full pt-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "bottom-end"}
-          <div :if={@show} class="absolute right-0 bottom-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute right-0 bottom-0 z-40">
             <div class="transform translate-y-full pt-2">
               <#slot name="content" />
             </div>
           </div>
         {#match "left-start"}
-          <div :if={@show} class="absolute left-0 top-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 top-0 right-0 z-40">
             <div class="absolute transform -translate-x-full">
               <div class="top-0 right-0 pr-2">
                 <#slot name="content" />
@@ -97,7 +97,7 @@ defmodule Moon.Components.PopoverV2 do
             </div>
           </div>
         {#match "left"}
-          <div :if={@show} class="absolute left-0 bottom-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 bottom-0 right-0 z-40">
             <div class="absolute transform -translate-x-full -translate-y-1/3">
               <div class="top-0 right-0 pr-2">
                 <#slot name="content" />
@@ -105,7 +105,7 @@ defmodule Moon.Components.PopoverV2 do
             </div>
           </div>
         {#match "left-end"}
-          <div :if={@show} class="absolute left-0 bottom-0 right-0 z-40">
+          <div :if={@show} :on-click={@on_close} class="absolute left-0 bottom-0 right-0 z-40">
             <div class="absolute transform -translate-x-full -translate-y-full">
               <div class="top-0 right-0 pr-2">
                 <#slot name="content" />
