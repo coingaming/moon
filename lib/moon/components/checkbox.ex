803ddd14d68be471babaf3a61e2327f7b0a32570
Anil Wadghule <anildigital@gmail.com>
2021-08-23 04:48:26 +0530

    Update checkbox styles

--- lib/moon/components/checkbox.ex
+++ lib/moon/components/checkbox.ex
@@ -11,14 +11,24 @@ defmodule Moon.Components.Checkbox do
     ~F"""
     {asset_import(@socket, "js/components/checkbox")}
 
-    <label class={"inline-flex items-center mt-3 #{@class}"}>
+    <label class={"relative inline-flex items-center mt-3 #{@class}"}>
       <input
         type="checkbox"
         checked={@checked}
         disabled={@disabled}
-        class="border disabled:text-hit-120 disabled:border-trunks-100 border-solid border-beerus-100 text-piccolo-100 focus:ring-transparent"
+        class="border disabled:text-hit-120  border-solid
+               border-trunks-100 disabled:border-beerus-100 text-piccolo-100 cursor-pointer focus:ring-transparent
+               whitespace-nowrap overflow-hidden"
       />
-      <span class="moon-checkbox-caption">
+      <span class="ml-[var(--space--default)] text-trunks-100 cursor-pointer inline-block before:after:m-space-default
+                after:m-space-default before:transition-duration-default
+                after:before:transition-duration-default before:ease-linear after:ease-linear
+                before:bg-goku-40 before:rounded-full before:opacity-0 before:transform-none
+                after:bg-transparent after:transition-colors before:left-0 after:left-0
+                transition before:transition-default after:transition-default
+                before:ease-in after:ease-in before:w-[var(--space--default)] after:w-[var(--space--default)]
+                before:h-[var(--space--default)] after:h-[var(--space--default)]
+                before:absolute after:absolute">
         <#slot />
       </span>
     </label>
