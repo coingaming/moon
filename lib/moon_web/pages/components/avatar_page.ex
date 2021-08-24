476606a86bdc563de67ad5e2074a27f1f9ae066c
Anil Wadghule <anildigital@gmail.com>
2021-08-24 19:36:49 +0530

    Style avatar component with tailwind css (#106)
    * Style avatar component with tailwind css; Update examples
    
    * Add default colors

--- lib/moon_web/pages/components/avatar_page.ex
+++ lib/moon_web/pages/components/avatar_page.ex
@@ -1,96 +1,115 @@
 defmodule MoonWeb.Pages.Components.AvatarPage do
   use MoonWeb, :live_view
+
   alias Moon.Components.Avatar
-  alias Moon.Components.Inline
   alias Moon.Components.CodePreview
+  alias Moon.Autolayouts.TopToDown
+  alias Moon.Components.Heading
+  alias Moon.Components.Link
+  alias MoonWeb.Components.ExampleAndCode
 
   def mount(params, _session, socket) do
     {:ok,
      assign(socket, theme_name: params["theme_name"] || "sportsbet-dark", active_page: __MODULE__)}
   end
 
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~F"""
-    <h1>Avatar</h1>
-    Component for displaying user profile image or placeholder if no image
+    <TopToDown>
+      <Heading size={32}>Avatar</Heading>
 
-    <a href="https://design.sportsbet.io/components/avatar">https://design.sportsbet.io/components/avatar</a>
+      <p>
+        <Link to="https://www.figma.com/file/S3q1SkVngbwHuwpxHKCsgtJj/Moon-Components?node-id=60%3A20">Figma design</Link>
+        <Link to="https://github.com/coingaming/moon/blob/master/lib/moon_web/pages/components/avatar_page.ex">Sourcecode of this page</Link>
+        <Link to="https://moon.io/components/avatar">React implementation</Link>
+      </p>
+      <Heading size={24} class="mt-4" is_regular>Usage</Heading>
+      Component for displaying user profile image or placeholder if no image
 
-    <h2>With user image</h2>
+      <Heading size={16}>Image avatars</Heading>
+      <ExampleAndCode>
+        <:example>
+          <div class="inline-flex">
+            <Avatar class="mx-10" image_url="//www.fillmurray.com/200/200" size="xsmall" />
+            <Avatar class="mx-10" image_url="//www.fillmurray.com/200/200" size="small" />
+            <Avatar class="mx-10" image_url="//www.fillmurray.com/200/200" size="medium" />
+            <Avatar class="mx-10" image_url="//www.fillmurray.com/200/200" size="large" />
+          </div>
+        </:example>
 
-    <Inline>
-      <Avatar image_url="//www.fillmurray.com/200/200" size="xsmall" />
-      <Avatar image_url="//www.fillmurray.com/200/200" size="small" />
-      <Avatar image_url="//www.fillmurray.com/200/200" size="medium" />
-      <Avatar image_url="//www.fillmurray.com/200/200" size="large" />
-    </Inline>
-
-    <#CodePreview>
-      <Inline>
+        <:code>
+          <#CodePreview>
         <Avatar image_url="//www.fillmurray.com/200/200" size="xsmall" />
         <Avatar image_url="//www.fillmurray.com/200/200" size="small" />
         <Avatar image_url="//www.fillmurray.com/200/200" size="medium" />
         <Avatar image_url="//www.fillmurray.com/200/200" size="large" />
-      </Inline>
     </#CodePreview>
+        </:code>
+      </ExampleAndCode>
 
-    <h2>With user name</h2>
+      <Heading size={16}>Letter avatars</Heading>
+      Use name prop. No more than 2 / 3 characters
 
-    Use name prop. No more than 2 / 3 characters
+      <ExampleAndCode>
+        <:example>
+          <div class="inline-flex">
+            <Avatar class="mx-10" name="JS" color="gohan-100" background_color="piccolo-100" size="xsmall" />
+            <Avatar class="mx-10" name="JS" color="gohan-100" background_color="piccolo-100" size="small" />
+            <Avatar class="mx-10" name="JS" color="gohan-100" background_color="piccolo-100" size="medium" />
+            <Avatar class="mx-10" name="JS" color="gohan-100" background_color="piccolo-100" size="large" />
+          </div>
+        </:example>
 
-    <Inline>
-      <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="xsmall" />
-      <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="small" />
-      <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="medium" />
-      <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="large" />
-    </Inline>
+        <:code>
+          <#CodePreview>
+        <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="xsmall" />
+        <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="small" />
+        <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="medium" />
+        <Avatar name="JS" color="gohan-100" background_color="piccolo-100" size="large" />
+        </#CodePreview>
+        </:code>
+      </ExampleAndCode>
 
-    <#CodePreview>
-      <Inline>
-        <Avatar
-          name="JS"
-          color="gohan-100"
-          background_color="piccolo-100"
-          size="xsmall"
-        />
-        <Avatar
-          name="JS"
-          color="gohan-100"
-          background_color="piccolo-100"
-          size="small"
-        />
-        <Avatar
-          name="JS"
-          color="gohan-100"
-          background_color="piccolo-100"
-          size="medium"
-        />
-        <Avatar
-          name="JS"
-          color="gohan-100"
-          background_color="piccolo-100"
-          size="large"
-        />
-      </Inline>
-    </#CodePreview>
+      <Heading size={16}>Without image and name (Fallback)</Heading>
+      <ExampleAndCode>
+        <:example>
+          <div class="inline-flex">
+            <Avatar class="mx-10" color="gohan-100" background_color="piccolo-100" size="xsmall" />
+            <Avatar class="mx-10" color="gohan-100" background_color="piccolo-100" size="small" />
+            <Avatar class="mx-10" color="gohan-100" background_color="piccolo-100" size="medium" />
+            <Avatar class="mx-10" color="gohan-100" background_color="piccolo-100" size="large" />
+          </div>
+        </:example>
 
-    <h2>Without image and name</h2>
-
-    <Inline>
-      <Avatar color="gohan-100" background_color="piccolo-100" size="xsmall" />
-      <Avatar color="gohan-100" background_color="piccolo-100" size="small" />
-      <Avatar color="gohan-100" background_color="piccolo-100" size="medium" />
-      <Avatar color="gohan-100" background_color="piccolo-100" size="large" />
-    </Inline>
-
-    <#CodePreview>
-      <Inline>
+        <:code>
+          <#CodePreview>
         <Avatar color="gohan-100" background_color="piccolo-100" size="xsmall" />
         <Avatar color="gohan-100" background_color="piccolo-100" size="small" />
         <Avatar color="gohan-100" background_color="piccolo-100" size="medium" />
         <Avatar color="gohan-100" background_color="piccolo-100" size="large" />
-      </Inline>
-    </#CodePreview>
+        </#CodePreview>
+        </:code>
+      </ExampleAndCode>
+
+      <Heading size={16}>Different colors</Heading>
+
+      <ExampleAndCode>
+        <:example>
+          <div class="inline-flex">
+            <Avatar class="mx-10" color="gohan-100" background_color="piccolo-100" size="medium" />
+            <Avatar class="mx-10" color="piccolo-100" background_color="gohan-100" size="medium" />
+          </div>
+        </:example>
+
+        <:code>
+          <#CodePreview>
+        <Avatar color="gohan-100" background_color="piccolo-100" size="medium" />
+        <Avatar color="piccolo-100" background_color="gohan-100" size="medium" />
+        </#CodePreview>
+        </:code>
+      </ExampleAndCode>
+    </TopToDown>
     """
   end
 end
