acbc4307e41a3329ff51ceb19c7eff36815dd3dd
Margus Pää-äär <53759446+margusp2heathmont@users.noreply.github.com>
2022-02-03 12:02:44 +0200

    This enables straight flow from fetching model to GUI (without remapp… (#205)
    * This enables straight flow from fetching model to GUI (without remapping).
    
    Usually models have id and name. After fetching it is kind of transpose... but we want less transposes and more direct flow.
    
    * Fixes
    
    * added string failover for value
    
    * made checkbox multiselect testable

--- lib/moon/components/checkbox_multiselect.ex
+++ lib/moon/components/checkbox_multiselect.ex
@@ -6,24 +6,49 @@ defmodule Moon.Components.CheckboxMultiselect do
   alias Moon.Components.Checkbox
   alias Moon.Components.ListItems.SingleLineItem
 
-  # [%{ label: :string, value: :string }, ...]
   prop options, :list, required: true
-  # [:string]
   prop values, :list, required: true
   prop on_select, :event
+  prop testid, :string
 
   def render(assigns) do
     ~F"""
     {#for option <- @options}
-      <div :on-click={@on_select} phx-value-toggled_item_id={option.value}>
+      <div
+        :on-click={@on_select}
+        phx-value-toggled_item_id={get_value(option)}
+        data-testid={"#{@testid}-value-#{get_value(option)}"}
+      >
         <SingleLineItem size="large">
           <:right_icon>
-            <Checkbox class="pointer-events-none" checked={Enum.member?(@values, option.value)} />
+            <Checkbox class="pointer-events-none" checked={is_checked(@values, option)} />
           </:right_icon>
-          {option.label}
+          {get_label(option)}
         </SingleLineItem>
       </div>
     {/for}
     """
+  end
+
+  def is_checked(values, option) do
+    Enum.member?(values, get_value(option)) || Enum.member?(values, "#{get_value(option)}")
+  end
+
+  def get_value(option) do
+    Map.get(option, :value) || Map.get(option, :id)
+  end
+
+  def get_label(option) do
+    Map.get(option, :label) || Map.get(option, :name)
+  end
+
+  def toggle_list_value(list, value) do
+    case Enum.member?(list, value) do
+      true ->
+        List.delete(list, value)
+
+      false ->
+        list ++ [value]
+    end
   end
 end
