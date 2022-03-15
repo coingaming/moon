deac515c261f120b44204c0ce6c09b2d1e12aeb3
Margus Part <margus.part@heathmont.net>
2022-03-16 00:36:27 +0200

    cleaned moon code

--- assets/js/listeners.js
+++ assets/js/listeners.js
@@ -11,18 +11,3 @@ window.addEventListener("moon:update-select", (event) => {
     new Event("input", { bubbles: true })
   )
 });
-
-// Phoenix:
-// JS.dispatch("moon:toggle-dropdown",
-//             detail: %{select_id: @id},
-//             to: "##{@id}"
-//           )
-
-// window.addEventListener("moon:toggle-dropdown", (event) => {
-//   var detail = event.detail;
-//   var selectId = detail.select_id;
-//   var dropdownId = selectId + "-dropdown";
-//   var dropdown = document.getElementById(dropdownId);
-//   dropdown.classList.toggle("hidden");
-//   dropdown.classList.toggle("absolute");
-// });
