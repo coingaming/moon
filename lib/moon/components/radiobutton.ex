796bfd8d3bdc82915c6dacb2bbf92270b7546d48
Alexis Azanza <alexis@devtailor.com>
2022-04-18 15:00:56 +0300

    Try to fix border radius

--- lib/moon/components/radiobutton.ex
+++ lib/moon/components/radiobutton.ex
@@ -31,7 +31,7 @@ defmodule Moon.Components.Radiobutton do
         }
       />
       <span class={
-        "radio-button-icon text-trunks-100 hover:hover-piccolo flex items-center justify-center circular w-6 h-6 min-w-[24px] min-h-[24px] relative mr-1 z-2 transition-[background-color] duration-[400ms]",
+        "radio-button-icon text-trunks-100 hover:hover-piccolo flex items-center justify-center rounded-full w-6 h-6 min-w-[24px] min-h-[24px] relative mr-1 z-2 transition-[background-color] duration-[400ms]",
         get_before_pseudo_styles(),
         get_after_pseudo_styles(),
         "opacity-[.35] cursor-not-allowed": @disabled,
@@ -47,14 +47,14 @@ defmodule Moon.Components.Radiobutton do
 
   defp get_before_pseudo_styles() do
     "before:content-[''] before:bg-piccolo-100 before:h-2 before:w-2 before:absolute
-    before:top-1/2 before:left-1/2 before:circular before:z-[2]
+    before:top-1/2 before:left-1/2 before:rounded-full before:z-[2]
     before:-translate-x-1/2 before:-translate-y-1/2 before:scale-0
     before:transition-all duration-300"
   end
 
   defp get_after_pseudo_styles() do
     "after:content-[''] after:h-4 after:w-4 after:bg-transparent after:absolute
-    after:top-1/2 after:left-1/2 after:circular
+    after:top-1/2 after:left-1/2 after:rounded-full
     after:-translate-x-1/2 after:-translate-y-1/2
     after:transition-[border-color] after:ease-in-out after:duration-200"
   end
