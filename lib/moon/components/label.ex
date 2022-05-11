0c7a9c96c05800119d5d67dce15b113905235a66
iamdnch <danil.chibrikov@heathmont.net>
2022-05-11 12:34:47 +0300

    default value for prop `class`

--- lib/moon/components/label.ex
+++ lib/moon/components/label.ex
@@ -6,12 +6,16 @@ defmodule Moon.Components.Label do
   prop color, :string, default: "gohan-100", values: Moon.colors()
   prop background_color, :string, default: "piccolo-100", values: Moon.colors()
   prop size, :string, default: "default", values: ["small", "default"]
-  prop class, :string
+  prop class, :string, default: "rounded-sm"
+  prop block_style_class, :string, default: "rounded-sm"
   slot default
 
   def render(assigns) do
     ~F"""
-    <div class={"#{@class} bg-#{@background_color} text-#{@color} pl-2 pr-2 rounded-sm #{(@size == "small" && "text-xxxs") || "text-xxs"} uppercase inline-block"}>
+    <div class={
+      @class,
+      "bg-#{@background_color} text-#{@color} pl-2 pr-2 #{(@size == "small" && "text-xxxs") || "text-xxs"} uppercase inline-block"
+    }>
       <#slot />
     </div>
     """
