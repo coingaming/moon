c9f1ba5b3e40b001e8e550a3d26ff84ce5199633
Danil Chibrikov <101857946+iamdanchi@users.noreply.github.com>
2022-05-13 16:22:31 +0300

    [PID-398] Removed `rounded` from hardcode (#260)
    * default value for prop `class`
    
    * removed wrong prop
    
    * add `border_radius_class` prop

--- lib/moon/components/label.ex
+++ lib/moon/components/label.ex
@@ -7,11 +7,19 @@ defmodule Moon.Components.Label do
   prop background_color, :string, default: "piccolo-100", values: Moon.colors()
   prop size, :string, default: "default", values: ["small", "default"]
   prop class, :string
+  prop border_radius_class, :string, default: "rounded-md"
   slot default
 
   def render(assigns) do
     ~F"""
-    <div class={"#{@class} bg-#{@background_color} text-#{@color} pl-2 pr-2 rounded-sm #{(@size == "small" && "text-xxxs") || "text-xxs"} uppercase inline-block"}>
+    <div class={
+      "#{@class} pl-2 pr-2 uppercase inline-block",
+      @border_radius_class,
+      "bg-#{@background_color}",
+      "text-#{@color}",
+      "text-xxxs": @size == "small",
+      "text-xxs": @size == "default"
+    }>
       <#slot />
     </div>
     """
