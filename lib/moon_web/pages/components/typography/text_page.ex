000a9272d71462b6c8dc1236869f08903a6bc5c7
Alexis Azanza <alexis@devtailor.com>
2022-05-31 00:47:06 +0300

    Text props

--- lib/moon_web/pages/components/typography/text_page.ex
+++ lib/moon_web/pages/components/typography/text_page.ex
@@ -10,6 +10,8 @@ defmodule MoonWeb.Pages.Components.Typography.TextPage do
   alias Moon.Components.Text
   alias MoonWeb.Components.ExampleAndCode
   alias MoonWeb.Components.Page
+  alias MoonWeb.Components.Table.Table
+  alias MoonWeb.Components.Table.Column
 
   data breadcrumbs, :any,
     default: [
@@ -72,22 +74,22 @@ defmodule MoonWeb.Pages.Components.Typography.TextPage do
 
           <:code>
             <#CodePreview>
-      <TopToDown>
-        <Text>Text with default font size</Text>
-        <Text size=10>Text with font size 10</Text>
-        <Text size=12>Text with font size 12</Text>
-        <Text size=14>Text with font size 14</Text>
-        <Text size=16>Text with font size 16</Text>
-        <Text size=18>Text with font size 18</Text>
-        <Text size=20>Text with font size 20</Text>
-        <Text size=24>Text with font size 24</Text>
-        <Text size=32>Text with font size 32</Text>
-        <Text size=48>font size 48</Text>
-        <Text size=56>font size 56</Text>
-        <Text size=64>font size 64</Text>
-        <Text size=72>font size 72</Text>
-      </TopToDown>
-    </#CodePreview>
+              <TopToDown>
+                <Text>Text with default font size</Text>
+                <Text size=10>Text with font size 10</Text>
+                <Text size=12>Text with font size 12</Text>
+                <Text size=14>Text with font size 14</Text>
+                <Text size=16>Text with font size 16</Text>
+                <Text size=18>Text with font size 18</Text>
+                <Text size=20>Text with font size 20</Text>
+                <Text size=24>Text with font size 24</Text>
+                <Text size=32>Text with font size 32</Text>
+                <Text size=48>font size 48</Text>
+                <Text size=56>font size 56</Text>
+                <Text size=64>font size 64</Text>
+                <Text size=72>font size 72</Text>
+              </TopToDown>
+            </#CodePreview>
           </:code>
         </ExampleAndCode>
 
@@ -105,13 +107,13 @@ defmodule MoonWeb.Pages.Components.Typography.TextPage do
           </:example>
           <:code>
             <#CodePreview>
-      <TopToDown>
-        <Text size=24>Text with default font weight</Text>
-        <Text size=24 is_bold=true>
-          Text with font-weight: semibold
-        </Text>
-      </TopToDown>
-    </#CodePreview>
+              <TopToDown>
+                <Text size=24>Text with default font weight</Text>
+                <Text size=24 is_bold=true>
+                  Text with font-weight: semibold
+                </Text>
+              </TopToDown>
+            </#CodePreview>
           </:code>
         </ExampleAndCode>
 
@@ -134,22 +136,94 @@ defmodule MoonWeb.Pages.Components.Typography.TextPage do
 
           <:code>
             <#CodePreview>
-      <TopToDown>
-        <Text size=24>Text with default color</Text>
-        <Text size=24 color="trunks_100">
-          Text with defined color
-        </Text>
-        <Text size=24 color="piccolo_100">
-          Text with defined color
-        </Text>
-        <Text size=24 color="krillin_100">
-          Text with defined color
-        </Text>
-        <Footer/>
-      </TopToDown>
-    </#CodePreview>
+              <TopToDown>
+                <Text size=24>Text with default color</Text>
+                <Text size=24 color="trunks_100">
+                  Text with defined color
+                </Text>
+                <Text size=24 color="piccolo_100">
+                  Text with defined color
+                </Text>
+                <Text size=24 color="krillin_100">
+                  Text with defined color
+                </Text>
+                <Footer/>
+              </TopToDown>
+            </#CodePreview>
           </:code>
         </ExampleAndCode>
+
+        <div>
+          <div class="text-bulma-100 items-center text-xl leading-7 font-normal my-4">TabLink Props Tabs</div>
+          <Table items={[
+            %{
+              :name => 'color',
+              :type => 'string',
+              :required => 'false',
+              :default => '-',
+              :description => 'Text color'
+            },
+            %{
+              :name => 'size',
+              :type => '9 | 10 | 12 | 14 | 16 | 18 | 20 | 24 | 32 | 48 | 56 | 64 | 72',
+              :required => 'false',
+              :default => '16',
+              :description => 'TODO - Text size (support size 9)'
+            },
+            %{
+              :name => 'is_bold',
+              :type => 'boolean',
+              :required => 'false',
+              :default => '-',
+              :description => 'Whether font weight is bold'
+            },
+            %{
+              :name => 'text_align',
+              :type => '-',
+              :required => '-',
+              :default => '-',
+              :description => 'TODO - Alignment'
+            },
+            %{
+              :name => 'is_uppercase',
+              :type => '-',
+              :required => '-',
+              :default => '-',
+              :description => 'TODO - Whether text is uppercase'
+            },
+            %{
+              :name => 'is_underline',
+              :type => '-',
+              :required => '-',
+              :default => '-',
+              :description => 'TODO - Whether text is underlined'
+            },
+            %{
+              :name => 'line_height',
+              :type => '-',
+              :required => '-',
+              :default => '-',
+              :description => 'TODO - Custom line height'
+            }
+
+          ]}>
+            <Column name="name" label="Name" :let={item: item} is_row_header>
+              {item.name}
+            </Column>
+            <Column name="type" label="Type" :let={item: item}>
+              {item.type}
+            </Column>
+            <Column name="required" label="Required" :let={item: item}>
+              {item.required}
+            </Column>
+            <Column name="default" label="Default" :let={item: item}>
+              {item.default}
+            </Column>
+            <Column name="description" label="Description" :let={item: item}>
+              {item.description}
+            </Column>
+          </Table>
+        </div>
       </TopToDown>
     </Page>
     """
