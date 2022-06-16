11b9b6062adb389d3fc29536a2445e2a96a1454c
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-06-16 15:33:17 +0300

    fix: format code [MDS_135]

--- lib/moon_web/components/example_and_code.ex
+++ lib/moon_web/components/example_and_code.ex
@@ -23,7 +23,7 @@ defmodule MoonWeb.Components.ExampleAndCode do
       <div class="my-2 text-moon-14" :if={slot_assigned?(:note)}>
         <p><#slot name="note" /></p>
       </div>
-      <Context get={theme_class: theme_class}>
+      <Context>
         <div class={
           "p-4 flex bg-goku-100 text-moon-14 rounded-moon-s-sm",
           "moon-design-dark hidden": @selected_button == "code"
