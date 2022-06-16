85340bae84ea797a865f2d0ba86992f0736c713a
Alexis Azanza <alexis@devtailor.com>
2022-06-16 10:33:35 +0300

    fix: fix the Sidebar/Left Menu styling and align with React [MDS-107] (#290)
    fix: fix the Sidebar/Left Menu styling and align with React [MDS-107] (#290)

---
+++ lib/moon_web/components/sidebar_link.ex
@@ -0,0 +1,27 @@
+defmodule MoonWeb.Components.SidebarLink do
+  @moduledoc false
+
+  use MoonWeb, :stateless_component
+
+  alias Moon.Components.Chip
+
+  prop route, :any
+  slot default
+
+  def render(assigns) do
+    ~F"""
+    <Context get={active_page: active_page, theme_name: theme_name}>
+      <a
+        data-phx-link="patch"
+        data-phx-link-state="push"
+        data-moon-active={active_page == @route}
+        href={live_path(MoonWeb.Endpoint, @route, theme_name: theme_name)}
+      >
+        <Chip active={active_page == @route} variant="ghost">
+          <#slot />
+        </Chip>
+      </a>
+    </Context>
+    """
+  end
+end
