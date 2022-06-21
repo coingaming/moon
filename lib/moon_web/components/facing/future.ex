0ebb517adb8897e0f7cfca17ceb79f90e82c1744
Alexis Azanza <alexis@devtailor.com>
2022-06-21 13:44:50 +0300

    Finished Future component

--- lib/moon_web/components/facing/future.ex
+++ lib/moon_web/components/facing/future.ex
@@ -9,8 +9,8 @@ defmodule MoonWeb.Components.Facing.Future do
     ~F"""
       <div>
         <a href="/howToContribute" class="group block relative">
-          <div class="relative h-[240px] w-[1176px] text-gohan-100">
-            <div class="flex absolute top-0 left-0 z-20 mx-14 my-6 w-[1176px]">
+
+            <div class="md:h-full text-gohan-100 flex box-border justify-between z-10 px-14 py-8 bg-[url('/moon/assets/images/facing/future_bg.png')] bg-cover rounded-moon-s-lg w-[1176px]">
               <div class="flex flex-col gap-6">
                 <h3 class="relative text-goten text-moon-48 font-medium z-1">
                   Want to contribute?
@@ -24,19 +24,10 @@ defmodule MoonWeb.Components.Facing.Future do
                 <FutureIcon />
               </div>
               <div class="relative text-goten z-1">
-                <ArrowsTopRight font_size="3rem"/>
+                <ArrowsTopRight font_size="2rem"/>
               </div>
             </div>
-            <div class="md:h-full absolute top-0 left-0 z-10 w-[1176px]">
-              <img
-                class="rounded-moon-s-lg object-left"
-                src="/moon/assets/images/facing/future_bg.png"
-                layout="fill"
-                objectFit="cover"
-                quality={100}
-              />
-            </div>
-          </div>
+
         </a>
       </div>
     """
