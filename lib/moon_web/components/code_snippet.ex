680196e850d5ba258d909d2df3e172da8cc295da
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-07-19 22:31:37 +0300

    fix: add getting started page [MDS-173] (#324)

---
+++ lib/moon_web/components/code_snippet.ex
@@ -0,0 +1,15 @@
+defmodule MoonWeb.Components.CodeSnippet do
+  @moduledoc false
+
+  use MoonWeb, :stateless_component
+
+  slot default
+
+  def render(assigns) do
+    ~F"""
+    <pre class="moon-design-dark w-full bg-goku-100 overflow-scroll p-4 text-moon-14 text-bulma-100 rounded-moon-s-sm">
+      <#slot />
+    </pre>
+    """
+  end
+end
