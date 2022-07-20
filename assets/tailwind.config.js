1e0eb1a07f0bb7709c716aee928026f6b9f7065e
Sergey Suprunenko <sergey.suprunenko@erlang-solutions.com>
2022-07-20 09:52:33 +0100

    feat: Remove tailwindcss/aspect-ratio plugin

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -176,7 +176,6 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
     plugin(({ addComponents }) => {
       addComponents({
