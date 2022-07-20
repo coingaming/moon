98f52d2c2f9f00abea5ce3e6d4dfd04b9fc91660
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-07-20 17:37:00 +0300

    fix: add how to contribute page [MDS-174] (#325)
    * fix: add getting started page [MDS-173]
    
    * fix: add how to contribute page [MDS-174]

--- lib/moon_web/pages/contribute_page.ex
+++ lib/moon_web/pages/contribute_page.ex
@@ -3,15 +3,15 @@ defmodule MoonWeb.Pages.ContributePage do
 
   use MoonWeb, :live_view
 
-  alias Moon.Autolayouts.TopToDown
-  alias Moon.Components.Heading
+  alias MoonWeb.Components.PageSection
+  alias MoonWeb.Components.CodeSnippet
   alias MoonWeb.Components.Page
 
   data breadcrumbs, :any,
     default: [
       %{
         to: "/contribute",
-        name: "Contribute"
+        name: "How to contribute"
       }
     ]
 
@@ -26,10 +26,21 @@ defmodule MoonWeb.Pages.ContributePage do
   def render(assigns) do
     ~F"""
     <Page theme_name={@theme_name} active_page={@active_page} breadcrumbs={@breadcrumbs}>
-      <TopToDown>
-        <Heading size={32}>Contribute</Heading>
-      </TopToDown>
-      Coming soon
+      <h1 class="text-moon-32 font-semibold">How to contribute</h1>
+      <PageSection title="First Section">
+        <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
+        <CodeSnippet>mix something</CodeSnippet>
+        <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
+        <CodeSnippet>mix something.else</CodeSnippet>
+      </PageSection>
+      <PageSection title="Second Section">
+        <p class="text-moon-16">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed quam at augue varius accumsan id in sapien. Suspendisse maximus tortor at urna blandit, et varius nibh maximus. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </p>
+        <CodeSnippet>iex -S mix phx.something</CodeSnippet>
+        <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
+        <CodeSnippet>iex -S mix phx.another</CodeSnippet>
+      </PageSection>
     </Page>
     """
   end
