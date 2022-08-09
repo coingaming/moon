8743e560f5545843f6a66e802a3349544f6361d1
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-08-09 12:30:49 +0300

    fix: add dynamic version number [MDS-186] (#343)
    * fix: temporary hide menu items [MDS-186]
    
    * fix: temporary hide menu items [MDS-186]
    
    * fix: add dynamic version number [MDS-186]
    
    * fix: add dynamic version number [MDS-186]

--- lib/moon_web/components/version.ex
+++ lib/moon_web/components/version.ex
@@ -8,8 +8,12 @@ defmodule MoonWeb.Components.Version do
     ~F"""
     <div class="flex text-moon-14 text-trunks-100 gap-2">
       <LogoMoonDesignShort height="1.5rem" width="1.5rem" />
-      <p>Version <span class="font-semibold">22.0.0</span></p>
+      <p>Version <span class="font-semibold">{get_version()}</span></p>
     </div>
     """
+  end
+
+  def get_version() do
+    File.read!("VERSION")
   end
 end
