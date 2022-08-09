15723d3eb86a2d12081973e0b0851f23af92b790
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-08-09 12:28:58 +0300

    fix: add dynamic version number [MDS-186]

--- lib/moon_web/components/version.ex
+++ lib/moon_web/components/version.ex
@@ -4,12 +4,18 @@ defmodule MoonWeb.Components.Version do
   use MoonWeb, :stateless_component
   alias Moon.Assets.Logos.LogoMoonDesignShort
 
+  prop version, :string
+
   def render(assigns) do
     ~F"""
     <div class="flex text-moon-14 text-trunks-100 gap-2">
       <LogoMoonDesignShort height="1.5rem" width="1.5rem" />
-      <p>Version <span class="font-semibold">22.0.0</span></p>
+      <p>Version <span class="font-semibold">{get_version()}</span></p>
     </div>
     """
+  end
+
+  def get_version() do
+    File.read!("VERSION")
   end
 end
