660678a9395f6cba71231271c6bc97f3c871f440
Margus Pää-äär <53759446+margusp2heathmont@users.noreply.github.com>
2022-08-15 17:58:50 +0300

    fix: update version (#351)

--- lib/moon_web/components/version.ex
+++ lib/moon_web/components/version.ex
@@ -8,7 +8,7 @@ defmodule MoonWeb.Components.Version do
     ~F"""
     <div class="flex text-moon-14 text-trunks-100 gap-2">
       <LogoMoonDesignShort height="1.5rem" width="1.5rem" />
-      <p>Version <span class="font-semibold">25.0.2</span></p>
+      <p>Version <span class="font-semibold">25.0.3</span></p>
     </div>
     """
   end
