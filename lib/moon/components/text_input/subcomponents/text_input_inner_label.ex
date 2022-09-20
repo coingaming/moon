f14a980ba13d02126cf5ab31ad93a43a9169d3a9
Alexis Azanza <alexis@devtailor.com>
2022-09-20 17:32:45 +0300

    fix: Fix for buttons on text input icons (#378)

--- lib/moon/components/text_input/subcomponents/text_input_inner_label.ex
+++ lib/moon/components/text_input/subcomponents/text_input_inner_label.ex
@@ -51,9 +51,9 @@ defmodule Moon.Components.TextInput.TextInputInnerLabel do
             "w-full absolute top-0 left-0 z-20 bg-transparent pointer-events-none",
             Utils.get_height_by_size(size)
           }>
-            <div class="w-full h-full flex items-center justify-between px-4 bg-transparent pointer-events-none">
-              <div class="justify-self-start"><#slot name="left_icon_slot" /></div>
-              <div class="justify-self-end"><#slot name="right_icon_slot" /></div>
+            <div class="w-full h-full flex items-center justify-between px-4 bg-transparent">
+              <div class="justify-self-start pointer-events-auto"><#slot name="left_icon_slot" /></div>
+              <div class="justify-self-end pointer-events-auto"><#slot name="right_icon_slot" /></div>
             </div>
           </div>
         </div>
