bc3b520e7eb01956de76dcfe6802bd991e17c33b
Alexis Azanza <alexis@devtailor.com>
2022-09-26 16:34:24 +0300

    Fix conflicts

--- lib/moon/components/list_items/single_line_item.ex
+++ lib/moon/components/list_items/single_line_item.ex
@@ -3,6 +3,7 @@ defmodule Moon.Components.ListItems.SingleLineItem do
 
   use Moon.StatelessComponent
 
+  prop size, :string, values: ~w(medium large), default: "medium"
   prop background_color, :string, values: Moon.colors(), default: "bg-gohan-100"
   prop active_background_color, :string, values: Moon.colors(), default: "bg-goku-100"
   prop current, :boolean, default: false
@@ -16,9 +17,11 @@ defmodule Moon.Components.ListItems.SingleLineItem do
   def render(assigns) do
     ~F"""
     <div class={
-      "relative rounded-moon-i-md text-moon-16 text-bulma-100 leading-6 cursor-pointer flex",
+      "relative rounded-moon-i-md text-moon-16 text-bulma-100 leading-6 cursor-pointer flex gap-4 p-4",
       @background_color,
       "hover:#{@active_background_color}",
+      "gap-2 p-2 py-2": @size == "medium",
+      "gap-4 p-4 py-3": @size == "large",
       "#{@active_background_color}": @current
     }>
       <div class={"flex items-center", grow: @left_grow} :if={slot_assigned?(:left_icon)}>
