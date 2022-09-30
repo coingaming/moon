79497a45d006491fc86ab52f03e2409396302693
Margus Part <margus.part@heathmont.net>
2022-09-30 12:27:51 +0300

    Update popover.js

--- assets/js/hooks/popover.js
+++ assets/js/hooks/popover.js
@@ -2,11 +2,10 @@ import { createPopper } from '@popperjs/core';
 
 export default {
     updated() {
-        window.el = this.el
         const trigger = this.el.children[0];
         const tooltip = this.el.children[1];
 
-        createPopper(trigger, tooltip, {
+        tooltip && createPopper(trigger, tooltip, {
             placement: this.el.dataset.placement || 'bottom-start',
             strategy: 'fixed'
         });
