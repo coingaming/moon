58f6a5fa1596ce51b047fc94530da1464bb3ba2d
Alexis Azanza <alexis@devtailor.com>
2022-10-24 11:15:48 +0300

    fix: align Breadcrumb docs with React [MDS-254] (#423)
    * Matched example titles with react
    
    * Corrected style for four items
    
    * Added example for one item crumb
    
    * Renamed current page
    
    * Run formatters
    
    * Updated example code
    
    * Remove unused code

--- lib/moon_web/pages/components/breadcrumb_page.ex
+++ lib/moon_web/pages/components/breadcrumb_page.ex
@@ -42,7 +42,7 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
         link: "/app/home"
       },
       %Crumb{
-        name: "Corporate",
+        name: "Current Page",
         link: "/app/corporate"
       }
     ]
@@ -66,8 +66,36 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
         link: "/app/corporate/about/jobs"
       },
       %Crumb{
-        name: "Contact",
+        name: "Current Page",
         link: "/app/corporate/about/jobs/contact"
+      }
+    ]
+
+  data breadcrumb_items_3, :list,
+    default: [
+      %Crumb{
+        name: "Home",
+        link: "/app/home"
+      },
+      %Crumb{
+        name: "Corporate",
+        link: "/app/corporate"
+      },
+      %Crumb{
+        name: "About",
+        link: "/app/corporate/about"
+      },
+      %Crumb{
+        name: "Current Page",
+        link: "/app/corporate/about/jobs"
+      }
+    ]
+
+  data breadcrumb_items_4, :list,
+    default: [
+      %Crumb{
+        name: "Current Page",
+        link: "/app/home"
       }
     ]
 
@@ -77,7 +105,27 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
       <TopToDown>
         <Heading size={32}>Breadcrumb</Heading>
 
-        <ExampleAndCode title="Extended" id="breadcrumb_1">
+        <ExampleAndCode title="Five or more items" id="breadcrumbs_2">
+          <:example>
+            <div class="flex justify-center items-center">
+              <Breadcrumb id="breadcrumb_2" breadcrumbs={@breadcrumb_items_2} />
+            </div>
+          </:example>
+
+          <:code>{breadcrumb_2_code()}</:code>
+        </ExampleAndCode>
+
+        <ExampleAndCode title="Four items" id="breadcrumbs_3">
+          <:example>
+            <div class="flex justify-center items-center py-1">
+              <Breadcrumb id="breadcrumb_3" breadcrumbs={@breadcrumb_items_3} />
+            </div>
+          </:example>
+
+          <:code>{breadcrumb_3_code()}</:code>
+        </ExampleAndCode>
+
+        <ExampleAndCode title="Two items" id="breadcrumbs_1">
           <:example>
             <div class="flex justify-center items-center py-1">
               <Breadcrumb id="breadcrumb_1" breadcrumbs={@breadcrumb_items_1} />
@@ -87,14 +135,14 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
           <:code>{breadcrumb_1_code()}</:code>
         </ExampleAndCode>
 
-        <ExampleAndCode title="Collapsed" id="breadcrumb_2">
+        <ExampleAndCode title="One item" id="breadcrumbs_4">
           <:example>
-            <div class="flex justify-center items-center">
-              <Breadcrumb id="breadcrumb_2" breadcrumbs={@breadcrumb_items_2} />
+            <div class="flex justify-center items-center py-1">
+              <Breadcrumb id="breadcrumb_4" breadcrumbs={@breadcrumb_items_4} />
             </div>
           </:example>
 
-          <:code>{breadcrumb_2_code()}</:code>
+          <:code>{breadcrumb_4_code()}</:code>
         </ExampleAndCode>
 
         <div>
@@ -131,7 +179,7 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
           link: "/app/home"
         },
         %Crumb{
-          name: "Corporate",
+          name: "Current Page",
           link: "/app/corporate"
         }
       ]
@@ -161,12 +209,50 @@ defmodule MoonWeb.Pages.Components.BreadcrumbPage do
           link: "/app/corporate/about/jobs"
         },
         %Crumb{
-          name: "Contact",
+          name: "Current Page",
           link: "/app/corporate/about/jobs/contact"
         }
       ]
 
-    <Breadcrumb id="breadcrumb_2" breadcrumbs={@breadcrumb_items_2} />
+    <Breadcrumb id="breadcrumb_3" breadcrumbs={@breadcrumb_items_3} />
+    """
+  end
+
+  def breadcrumb_3_code do
+    """
+    data breadcrumb_items_3, :list,
+      default: [
+        %Crumb{
+          name: "Home",
+          link: "/app/home"
+        },
+        %Crumb{
+          name: "Corporate",
+          link: "/app/corporate"
+        },
+        %Crumb{
+          name: "About",
+          link: "/app/corporate/about"
+        },
+        %Crumb{
+          name: "Current Page",
+          link: "/app/corporate/about/jobs"
+        }
+      ]
+    """
+  end
+
+  def breadcrumb_4_code do
+    """
+    data breadcrumb_items_4, :list,
+      default: [
+        %Crumb{
+          name: "Current Page",
+          link: "/app/home"
+        }
+      ]
+
+    <Breadcrumb id="breadcrumb_4" breadcrumbs={@breadcrumb_items_4} />
     """
   end
 end
