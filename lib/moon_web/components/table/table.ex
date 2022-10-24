5c98a60f497e016635f75b69fb43c71b6c4c4de2
Sergey Semenov <phgrey@gmail.com>
2022-10-24 20:23:27 +0300

    text-color in table cross-side fix [MDS-230] (#427)

--- lib/moon_web/components/table/table.ex
+++ lib/moon_web/components/table/table.ex
@@ -11,13 +11,13 @@ defmodule MoonWeb.Components.Table.Table do
 
   def render(assigns) do
     ~F"""
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto text-bulma-100">
       <table class="min-w-full divide-y divide-beerus-100">
         <thead class="bg-goku-100">
           <tr>
             {#for col <- @cols}
               <th
-                class="px-4 py-3 text-left text-moon-12 font-semibold text-trunks-100 uppercase"
+                class="px-4 py-3 text-left text-moon-12 font-semibold uppercase"
                 data-testid={"sort-column-#{col.name}"}
               >
                 {col.label}
@@ -34,8 +34,8 @@ defmodule MoonWeb.Components.Table.Table do
               {#for {_, col_index} <- Enum.with_index(@cols)}
                 <td
                   class={
-                    "px-4 py-4 whitespace-nowrap text-moon-14 text-trunks-100 text-left",
-                    "text-bulma-100 font-semibold": col_index == 0
+                    "px-4 py-4 whitespace-nowrap text-moon-14 text-left",
+                    "font-semibold": col_index == 0
                   }
                   data-testid={"row-#{row_index}-col-#{col_index}"}
                 >
