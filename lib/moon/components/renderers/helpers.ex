6d516a7251a3fb9bca65ea1246fb0322289506cd
Margus Part <margus.part@heathmont.net>
2022-11-09 13:49:14 +0200

    Update helpers.ex

--- lib/moon/components/renderers/helpers.ex
+++ lib/moon/components/renderers/helpers.ex
@@ -1,8 +1,6 @@
 defmodule Moon.Components.Renderers.Helpers do
   @moduledoc false
 
-  Moon.Components.Renderers.Datetime
-
   def datetime(value) do
     value && Timex.format!(value, "%b %d, %Y, %H:%M:%S", :strftime)
   end
