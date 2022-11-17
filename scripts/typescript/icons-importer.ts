ef18d9f8e89741359a87fc84e242f59958056958
Sergey Semenov <phgrey@gmail.com>
2022-11-17 09:57:29 +0200

    reverting icons importer fix

--- scripts/typescript/icons-importer.ts
+++ scripts/typescript/icons-importer.ts
@@ -33,7 +33,7 @@ const propsMap = `
   prop background_color, :string, values: Moon.colors
   prop font_size, :string
   prop click, :event
-  prop class, :css_class
+  prop class, :string
 `;
 
 const propsMapKeys = [
@@ -82,8 +82,7 @@ const createAssetComponentFile = ({ file }: CreateAssetsComponentFileProps) => {
     .toLowerCase()}.ex`;
 
   const svgMap = `
-
-  <svg {"moon-icon", @class, "cursor-pointer": !!@click} :on-click={@click} style={get_style(color: @color, background_color: @background_color, font_size: @font_size)}>
+  <svg class={"moon-icon #{@class} #{@click && "cursor-pointer"}"} :on-click={@click} style={get_style(color: @color, background_color: @background_color, font_size: @font_size)}>
     <use href="/moon/assets/svgs/icons_new/${file}.svg#item"></use>
   </svg>
   `;
