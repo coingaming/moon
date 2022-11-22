cbbb5b0c5a3fcbfc1062995d3603d6e944291131
Alexis Azanza <alexis@devtailor.com>
2022-11-22 08:49:42 +0200

    Fix tests

--- lib/moon/components/lego/checvron_up_down.ex
+++ lib/moon/components/lego/checvron_up_down.ex
@@ -3,14 +3,18 @@ defmodule Moon.Components.Lego.ChevronUpDown do
 
   use Moon.StatelessComponent
 
-  alias Moon.Icons.ControlsChevronDown
+  # TODO
+  # Use the original icon after https://github.com/coingaming/moon/pull/450 is merged
+  # and class is changed from :string to :css_class
+  # alias Moon.Icons.ControlsChevronDown
+  alias Moon.Components.Lego.TempControlsChevronDown
 
   prop(is_selected, :boolean, from_context: {Moon.Components.Lego, :is_selected})
 
   def render(assigns) do
     ~F"""
     <span>
-      <ControlsChevronDown class={
+      <TempControlsChevronDown class={
         "transition-transform transition-200",
         "rotate-180": @is_selected
       } />
