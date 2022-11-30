06a80f0a9650591837f3a49a78afafbd1ade0d72
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2022-11-30 10:31:20 +0200

    Additional alignment of menu items on Sidebar with React TW

--- lib/moon_web/components/header.ex
+++ lib/moon_web/components/header.ex
@@ -4,17 +4,18 @@ defmodule MoonWeb.Components.Header do
   use MoonWeb, :stateless_component
 
   alias Moon.Assets.Logos.LogoMoonDesignShort
+  alias Moon.Icons.GenericMenu
 
   prop(click, :event)
   slot(default)
 
   def render(assigns) do
     ~F"""
-    <header>
+    <header class="fixed top-0 bg-gohan z-50 px-5 w-full">
       <div class="relative z-10 py-4 lg:hidden flex flex-row gap-2 items-center justify-between text-bulma">
         <button type="button" class="focus:outline-none" :on-click={@click}>
           <span class="sr-only">Open sidebar</span>
-          <img src={static_path(MoonWeb.Endpoint, "/svgs/moon_web/left_menu.svg")}>
+          <GenericMenu class="text-moon-32"/>
         </button>
         <a href="/">
           <LogoMoonDesignShort height="2em" width="2em" />
