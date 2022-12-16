e45e84734adb3c52cb6f8148459eec06728ffb38
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2022-12-16 09:00:09 +0200

    How to contribute page initial updates

--- lib/moon_web/pages/contribute_page.ex
+++ lib/moon_web/pages/contribute_page.ex
@@ -20,19 +20,24 @@ defmodule MoonWeb.Pages.ContributePage do
     ~F"""
     <Page {=@theme_name} {=@active_page} {=@breadcrumbs} {=@direction}>
       <h1 class="text-moon-32 font-semibold">How to contribute</h1>
-      <PageSection title="First Section">
+      <PageSection title="Setup">
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>mix something</CodeSnippet>
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>mix something.else</CodeSnippet>
       </PageSection>
-      <PageSection title="Second Section">
+      <PageSection title="Check our Github">
         <p class="text-moon-16">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed quam at augue varius accumsan id in sapien. Suspendisse maximus tortor at urna blandit, et varius nibh maximus. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus.
         </p>
         <CodeSnippet>iex -S mix phx.something</CodeSnippet>
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>iex -S mix phx.another</CodeSnippet>
+      </PageSection>
+      <PageSection title="Check our Github">
+        <li class="text-moon-16">
+          Lorem ipsum
+        </li>
       </PageSection>
     </Page>
     """
