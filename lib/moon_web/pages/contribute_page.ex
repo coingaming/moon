ed765707508356e980acb36ce83d792b29607862
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2022-12-16 09:39:38 +0200

    Reverted initial changes on Contribute page, not

--- lib/moon_web/pages/contribute_page.ex
+++ lib/moon_web/pages/contribute_page.ex
@@ -20,24 +20,19 @@ defmodule MoonWeb.Pages.ContributePage do
     ~F"""
     <Page {=@theme_name} {=@active_page} {=@breadcrumbs} {=@direction}>
       <h1 class="text-moon-32 font-semibold">How to contribute</h1>
-      <PageSection title="Setup">
+      <PageSection title="First Section">
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>mix something</CodeSnippet>
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>mix something.else</CodeSnippet>
       </PageSection>
-      <PageSection title="Check our Github">
+      <PageSection title="Second Section">
         <p class="text-moon-16">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed quam at augue varius accumsan id in sapien. Suspendisse maximus tortor at urna blandit, et varius nibh maximus. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus.
         </p>
         <CodeSnippet>iex -S mix phx.something</CodeSnippet>
         <p class="text-moon-16">Lorem ipsum dolor sit amet</p>
         <CodeSnippet>iex -S mix phx.another</CodeSnippet>
-      </PageSection>
-      <PageSection title="Check our Github">
-        <li class="text-moon-16">
-          Lorem ipsum
-        </li>
       </PageSection>
     </Page>
     """
