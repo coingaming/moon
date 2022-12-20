acde8a295a1c70e4691ba3a5aceeb5f5b9c7574a
Sergey Semenov <phgrey@gmail.com>
2022-12-20 12:27:26 +0200

    fix: small table alignings (#480)

---
+++ lib/moon/design/table/column.ex
@@ -0,0 +1,11 @@
+defmodule Moon.Design.Table.Column do
+  @moduledoc false
+
+  use Surface.Component, slot: "cols"
+
+  @doc "The title of the column"
+  prop(name, :string)
+  prop(label, :string)
+  prop(sortable, :boolean, default: true)
+  prop(width, :css_class)
+end
