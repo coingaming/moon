03eefbee0d94b40cfd800e44582d949509e678af
phgrey <phgrey@gmail.com>
2023-01-16 19:54:58 +0200

    chore: more forgotten icons & paths

--- lib/moon/components/country_flag.ex
+++ lib/moon/components/country_flag.ex
@@ -17,7 +17,7 @@ defmodule Moon.CountryFlag do
       class={"moon-country-flag", @class, "cursor-pointer": @click}
       :on-click={@click}
       style={get_style(color: @color, background_color: @background_color, font_size: @font_size)}
-      src={"/moon/assets/svgs/country_flags/#{country_flag_name_to_filename(@name)}.svg"}
+      src={"/moon_icons/svgs/country_flags/#{country_flag_name_to_filename(@name)}.svg"}
     />
     """
   end
--- lib/moon/components/lego/temp_control_chevron_down.ex
+++ lib/moon/components/lego/temp_control_chevron_down.ex
@@ -21,7 +21,7 @@ defmodule Moon.Components.Lego.TempControlsChevronDown do
       :on-click={@click}
       style={get_style(color: @color, background_color: @background_color, font_size: @font_size)}
     >
-      <use href="/moon/assets/svgs/icons_new/controls-chevron-down.svg#item" />
+      <use href="/moon_icons/svgs/icons_new/controls-chevron-down.svg#item" />
     </svg>
     """
   end
