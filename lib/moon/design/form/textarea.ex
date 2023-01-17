0f25bd155c34323eeb70b1ea7b8a6ead088ec99c
phgrey <phgrey@gmail.com>
2023-01-17 17:56:02 +0200

    Textarea v2

---
+++ lib/moon/design/form/textarea.ex
@@ -0,0 +1,19 @@
+defmodule Moon.Design.Form.Textarea do
+  @moduledoc false
+
+  use Moon.StatelessComponent
+
+  slot(default)
+
+  prop(class, :css_class)
+  prop(attrs, :map, default: %{})
+
+  def render(assigns) do
+    ~F"""
+    <textarea class={merge(["appearance-none resize-none w-full p-4 text-moon-16 text-bulma bg-gohan",
+      "rounded-moon-s-sm placeholder:text-trunks outline outline-1 outline-offset-[-1px] outline-beerus",
+      "transition-all focus:outline-2 focus:outline-offset-[-2px] focus:outline-piccolo hover:outline-1",
+      "hover:outline-offset-[-1px] hover:outline-bulma/[0.12]", @class])} {...@attrs}><#slot /></textarea>
+    """
+  end
+end
