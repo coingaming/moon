b7a947b7e2f67660671d73a53711ab7cfc59bbee
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-01-18 13:33:34 +0200

    fixes

--- mix.exs
+++ mix.exs
@@ -79,8 +79,7 @@ defmodule Moon.MixProject do
       {:surface, "~> 0.9.1"},
       {:timex, "~> 3.6"},
       {:distillery, "~> 2.1"},
-      # {:moon_icons, git: "git@github.com:coingaming/moon-icons.git", branch: "main"},
-      {:moon_icons, path: "../moon-icons", override: true},
+      {:moon_icons, git: "git@github.com:coingaming/moon-icons.git", branch: "main"},
 
       # test
       {:excoveralls, "~> 0.10", only: :test},
