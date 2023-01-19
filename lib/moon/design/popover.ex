97ccab291e11a147b910cd23510505f80aaad727
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-01-19 22:41:33 +0200

    added aria + formatting

--- lib/moon/design/popover.ex
+++ lib/moon/design/popover.ex
@@ -1,62 +1,43 @@
 defmodule Moon.Design.Popover do
   @moduledoc false
 
-  use Moon.StatelessComponent
+  use Moon.StatefulComponent
+
+  prop(on_click, :event)
+  prop(close, :event)
+  prop(is_open, :boolean, default: false)
 
   slot(default, required: true)
   slot(trigger, required: true)
 
   def render(assigns) do
     ~F"""
-    <div class="relative inline-block group">
-      <div
-        role="tooltip"
-        class="hidden group-hover:block pointer-events-none transition-opacity transition-200"
-      >
+    <div
+      class="relative inline-block"
+      ,
+      :on-click={@on_click || "toggle_open"}
+      :on-click-away={@close || "close_panel"}
+    >
+      <div class={"transition-opacity transition-200", hidden: @is_open == false}>
         <#slot />
       </div>
-      <div aria-describedby="tooltip" class="inline-block">
+      <div class="inline-block">
         <#slot {@trigger} />
       </div>
     </div>
     """
   end
+
+  def handle_event("toggle_open", _, socket) do
+    if socket.assigns[:is_open] == false do
+      {:noreply, assign(socket, is_open: true)}
+    else
+      {:noreply, assign(socket, is_open: false)}
+    end
+  end
+
+  def handle_event("close_panel", _, socket) do
+    socket = assign(socket, is_open: false)
+    {:noreply, socket}
+  end
 end
-
-
-# defmodule Moon.Design.Popover do
-#   @moduledoc false
-
-#   use Moon.StatelessComponent
-
-#   slot(default, required: true)
-#   slot(trigger, required: true)
-
-#   def render(assigns) do
-#     ~F"""
-#     <div class="relative inline-block" phx-click-away={close}>
-#       <div
-#         class="transition-opacity transition-200"
-#       >
-#         <#slot />
-#       </div>
-#       <div class="inline-block" phx-click={toggle_popover}>
-#         <#slot {@trigger} />
-#       </div>
-#     </div>
-#     """
-#   end
-
-#   def handle_event("close", _params, socket) do
-#     socket = assign(socket, open: false)
-#     {:noreply, socket}
-#   end
-
-#   def handle_event("toggle_open", _params, socket) do
-#     if socket.assigns.disabled == false do
-#       {:noreply, assign(socket, open: !socket.assigns.open)}
-#     else
-#       {:noreply, socket}
-#     end
-#   end
-# end
