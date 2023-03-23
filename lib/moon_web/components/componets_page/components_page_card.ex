41ffe0bcaa45cc3e2d7ec7800732bc4061f3aee1
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-03-23 11:21:39 +0200

    horizontal padding on card fixed

--- lib/moon_web/components/componets_page/components_page_card.ex
+++ lib/moon_web/components/componets_page/components_page_card.ex
@@ -14,7 +14,7 @@ defmodule MoonWeb.Components.ComponetsPage.ComponentsPageCard do
       <a href={@href}>
         <div class="flex flex-col gap-4 border border-beerus rounded-moon-s-lg p-2 pb-6">
           <img src={@image} alt={@title} width={408} height={224}>
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-2 px-4">
             <h3 class="text-moon-20 font-medium">
               {@title}</h3>
             <p class="h-12 text-moon-16 text-ellipsis overflow-hidden">
