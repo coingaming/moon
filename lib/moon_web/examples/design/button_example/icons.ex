3e6157d1a969b8a9f7bb9e2575c27ff34f886db7
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-03-30 17:55:01 +0300

    adding examples to Button

--- lib/moon_web/examples/design/button_example/icons.ex
+++ lib/moon_web/examples/design/button_example/icons.ex
@@ -4,23 +4,41 @@ defmodule MoonWeb.Examples.Design.ButtonExample.Icons do
   use Moon.StatelessComponent
   use MoonWeb, :example
 
+  alias Moon.Icons.GenericSettings
   alias Moon.Design.Button
 
   def render(assigns) do
     ~F"""
-    <Button left_icon="generic_settings">Icon Left</Button>
-    <Button right_icon="generic_settings">Icon Right</Button>
+    <div class="flex flex-wrap justify-around w-full">
+      <Button left_icon="generic_settings">Icon Left</Button>
+      <Button right_icon="generic_settings">Icon Right</Button>
+    </div>
+    <div class="flex flex-wrap justify-around w-full">
+      <Button class="bg-roshi"><GenericSettings /> Icon inside
+      </Button>
+      <Button class="bg-krillin">Icon inside<GenericSettings class="text-moon-32 text-whis" />
+      </Button>
+    </div>
     """
   end
 
   def code() do
     """
     alias Moon.Design.Button
+    alias Moon.Icons.GenericSettings
 
     ...
 
-    <Button left_icon="generic_settings">Icon Left</Button>
-    <Button right_icon="generic_settings">Icon Right</Button>
+    <div class="flex flex-wrap justify-around w-full">
+      <Button left_icon="generic_settings">Icon Left</Button>
+      <Button right_icon="generic_settings">Icon Right</Button>
+    </div>
+    <div class="flex flex-wrap justify-around w-full">
+      <Button class="bg-roshi"><GenericSettings /> Icon inside
+      </Button>
+      <Button class="bg-krillin">Icon inside<GenericSettings class="text-moon-32 text-whis" />
+      </Button>
+    </div>
     """
   end
 end
