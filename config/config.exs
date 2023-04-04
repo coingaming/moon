ec4641dac53916c9e9f722160b44d7299705158b
Sergey Semenov <phgrey@gmail.com>
2023-04-04 23:52:46 +0300

    fix: surface config propagating for moon users

--- config/config.exs
+++ config/config.exs
@@ -30,31 +30,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# this file cn be used by our package users, so ...
+import_config "surface.exs"
+
 config :surface, :components, [
-  {Surface.Components.Form.ErrorTag,
-   default_translator: {MoonWeb.ErrorHelpers, :translate_error}},
-  # lib part
-  {Moon.Components.Form, propagate_context_to_slots: true},
-  {Moon.Components.Field, propagate_context_to_slots: true},
-  {Moon.Components.TextInput, propagate_context_to_slots: true},
-  {Moon.Components.Select.Dropdown, propagate_context_to_slots: true},
-  {Moon.Design.MenuItem, propagate_context_to_slots: true},
-  {Moon.Design.Tabs, propagate_context_to_slots: true},
-  {Moon.Design.Tabs.List, propagate_context_to_slots: true},
-  {Moon.Design.Tooltip.Content, propagate_context_to_slots: true},
-  {Moon.Design.Accordion, propagate_context_to_slots: true},
-  {Moon.Design.Accordion.Item, propagate_context_to_slots: true},
-  {Moon.Design.Modal, propagate_context_to_slots: true},
-  {Moon.Design.Modal.Panel, propagate_context_to_slots: true},
-  {Moon.Design.Dropdown, propagate_context_to_slots: true},
-  {Moon.Design.Snackbar, propagate_context_to_slots: true},
-  {Moon.Design.Snackbar.Close, propagate_context_to_slots: true},
-  {Moon.Design.Form, propagate_context_to_slots: true},
-  {Moon.Design.Form.Field, propagate_context_to_slots: true},
-  {MoonWeb.Components.ExampleAndCode, propagate_context_to_slots: true},
-  {Moon.Design.Drawer, propagate_context_to_slots: true},
-  # app part
-  {MoonWeb.Components.SidebarLink, propagate_context_to_slots: false}
+  # put here configs that are not required by our users
 ]
 
 # Import environment specific config. This must remain at the bottom
