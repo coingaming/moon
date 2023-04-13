c91194457edf7596de7ba9a9840092ae554859d7
phgrey <phgrey@gmail.com>
2023-04-13 12:32:58 +0300

    switch example fix

--- lib/moon_web/examples/design/switch_example/using_with_forms.ex
+++ lib/moon_web/examples/design/switch_example/using_with_forms.ex
@@ -30,7 +30,29 @@ defmodule MoonWeb.Examples.Design.SwitchExample.UsingWithForms do
 
   def code() do
     """
+    alias Moon.Design.Form.Switch
+    alias Moon.Design.Form
+    alias Moon.Design.Form.Field
+    alias MoonWeb.Pages.Tutorials.AddDataUsingForm.User
 
+    prop(user_changeset, :any, default: User.changeset(%User{}))
+
+    def handle_event("change", %{"user" => params}, socket) do
+      user_changeset = User.changeset(%User{}, params)
+      {:noreply, assign(socket, user_changeset: user_changeset)}
+    end
+
+    def render(assigns) do
+      ~F\"""
+      <div>
+        <Form for={@user_changeset} change="change">
+          <Field field={:notification}>
+            <Switch />
+          </Field>
+        </Form>
+      </div>
+      \"""
+    end
     """
   end
 end
