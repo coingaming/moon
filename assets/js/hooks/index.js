937f58f247976a18567ca9a0bcb1a600ea134207
phgrey <phgrey@gmail.com>
2023-06-14 10:47:52 +0300

    fix: moving Carousel hook to surface format

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,7 +1,6 @@
 import Animation from './animation'
 import Datepicker from './datepicker'
 import Bottomsheet from './bottom_sheet'
-import Carousel from './carousel'
 import Combobox from './combobox'
 import Authcode from './auth_code'
 
@@ -9,7 +8,6 @@ export default {
   Animation,
   Datepicker,
   Bottomsheet,
-  Carousel,
   Combobox,
   Authcode
 }
