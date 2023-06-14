d19b4fce9e3e5b43e5fe8f808d78398f7aae29f8
Margus Pärt <53759446+margusp2heathmont@users.noreply.github.com>
2023-06-14 14:07:40 +0300

    fix: snackbar and add autoclose (#659)

--- lib/moon/design/snackbar.ex
+++ lib/moon/design/snackbar.ex
@@ -20,9 +20,11 @@ defmodule Moon.Design.Snackbar do
   slot(icon)
   slot(close)
 
-  prop(is_open, :boolean, default: false)
+  prop(is_open, :boolean)
   prop(class, :css_class)
+  prop(outer_class, :css_class)
   prop(timeout, :integer, default: 5000)
+  prop(autoclose, :boolean, default: true)
 
   prop(testid, :string)
 
@@ -34,41 +36,54 @@ defmodule Moon.Design.Snackbar do
     {:noreply, assign(socket, is_open: false)}
   end
 
-  defp animate_class(position) do
-    case position do
-      "top-left" -> "animate-leftslide"
-      "bottom-left" -> "animate-leftslide"
-      "top-center" -> "animate-topslide"
-      "bottom-center" -> "animate-bottomslide"
-      "top-right" -> "animate-rightslide"
-      "bottom-right" -> "animate-rightslide"
-    end
-  end
+  @animate_class_map %{
+    "top-left" => "animate-leftslide",
+    "bottom-left" => "animate-leftslide",
+    "top-center" => "animate-topslide",
+    "bottom-center" => "animate-bottomslide",
+    "top-right" => "animate-rightslide",
+    "bottom-right" => "animate-rightslide"
+  }
+
+  defp animate_class(position), do: Map.get(@animate_class_map, position)
+
+  @position_class_map %{
+    "top-left" => "top-4 left-4",
+    "top-center" => "justify-center top-4 left-4 right-4 md:m-auto",
+    "top-right" => "justify-end top-4 right-4",
+    "bottom-left" => "bottom-4 left-4",
+    "bottom-center" => "justify-center bottom-4 left-4 right-4 m-auto",
+    "bottom-right" => "justify-end bottom-4 right-4"
+  }
+
+  defp position_class(position), do: Map.get(@position_class_map, position)
 
   def render(assigns) do
     ~F"""
-    <div>
-      <div :on-click="set_open">
-        <#slot {@trigger} />
-      </div>
+    <div
+      id={@id}
+      data-testid={@testid || @id}
+      data-is_open={@is_open && "true"}
+      data-autoclose={@autoclose && "true"}
+      data-timeout={@timeout}
+      data-animate_class={animate_class(@position)}
+      data-snackbar="true"
+      :hook="default"
+    >
+      {#if slot_assigned?(:trigger)}
+        <div id={@id <> "-trigger"} data-testid={"#{@testid || @id}-trigger"} :on-click="set_open">
+          <#slot {@trigger} />
+        </div>
+      {/if}
       <div
-        data-is_open={@is_open}
-        data-animate_class={animate_class(@position)}
-        data-timeout={@timeout}
+        id={@id <> "-content"}
         class={merge([
           "z-[9999999] flex fixed w-[calc(100%-32px)] md:w-fit transition hidden",
-          "moon-snackbar",
-          "top-4 left-4": @position == "top-left",
-          "justify-center top-4 left-4 right-4 md:m-auto": @position == "top-center",
-          "justify-end top-4 right-4": @position == "top-right",
-          "bottom-4 left-4": @position == "bottom-left",
-          "justify-center bottom-4 left-4 right-4 m-auto": @position == "bottom-center",
-          "justify-end bottom-4 right-4": @position == "bottom-right"
+          position_class(@position),
+          @outer_class
         ])}
-        id={"#{@id}-snack"}
-        data-testid={"#{@testid}-snackbar"}
+        data-testid={"#{@testid || @id}-content"}
         aria-hidden={(@is_open && "true") || "false"}
-        :hook="default"
       >
         <div class={merge([
           "flex w-fit max-w-xs items-center gap-4 p-4 bg-gohan shadow-moon-lg rounded-moon-s-sm",
