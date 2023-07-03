be183fb8e86c99abacb244ef477af9a0e4d1e128
Artur Kasza <artur.kasza@yolo.com>
2023-07-03 12:38:01 +0200

    fix: Add testid to Dropdown.Trigger (#671)

--- lib/moon/design/dropdown/trigger.ex
+++ lib/moon/design/dropdown/trigger.ex
@@ -9,12 +9,15 @@ defmodule Moon.Design.Dropdown.Trigger do
   prop(is_open, :boolean, from_context: :is_open)
   @doc "Event that fired when trigger is clicked"
   prop(on_click, :event, from_context: :on_trigger)
+  @doc "Data-testid attribute for HTML tag"
+  prop(testid, :string)
+
   @doc "Inner content of the component"
   slot(default)
 
   def render(assigns) do
     ~F"""
-    <div :on-click={@on_click} aria-haspopup="true" aria-expanded={@is_open}>
+    <div :on-click={@on_click} data-testid={@testid} aria-haspopup="true" aria-expanded={@is_open}>
       <#slot>{@title}</#slot>
     </div>
     """
