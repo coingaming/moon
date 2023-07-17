2842b6593ff5c9decf4cdb4bf0a41e4d7666a3ab
Margus Pärt <53759446+margusp2heathmont@users.noreply.github.com>
2023-07-17 20:00:30 +0300

    Update sidebar.ex

--- lib/moon/design/sidebar.ex
+++ lib/moon/design/sidebar.ex
@@ -2,7 +2,8 @@ defmodule Moon.Design.Sidebar do
   use Moon.StatefulComponent
 
   alias Moon.Design.Drawer
-  data(is_open, :boolean, default: true) # TODO this is copy from MoonWeb but seems hacky
+  # TODO this is copy from MoonWeb but seems hacky
+  data(is_open, :boolean, default: true)
   prop(toggle_is_collapsed, :event)
   prop(is_collapsed, :boolean, default: true)
   slot(top)
@@ -12,10 +13,24 @@ defmodule Moon.Design.Sidebar do
   def render(assigns) do
     ~F"""
     <div id={@id}>
-      <Drawer lg_persists is_open id={@id <> "-drawer"} class={"w-full lg:flex lg:start-0 lg:z-50 lg:right-[unset]", "lg:w-[280px]": !@is_collapsed}>
+      <Drawer
+        lg_persists
+        is_open
+        id={@id <> "-drawer"}
+        class={"w-full lg:flex lg:start-0 lg:z-50 lg:right-[unset]", "lg:w-[280px]": !@is_collapsed}
+      >
         <Drawer.Backdrop class="lg:hidden" />
-        <Drawer.Panel position="start" class={"sidebar-panel w-full lg:flex lg:start-0 lg:z-50 w-auto lg:right-[unset]", "lg:w-[280px]": !@is_collapsed}>
-          <div :on-click={@toggle_is_collapsed} class="rounded-full shadow-md -right-3 top-6 bg-inherit w-6 h-6 cursor-pointer sidebar-panel-chevron hidden z-[10001]">
+        <Drawer.Panel
+          position="start"
+          class={
+            "sidebar-panel w-full lg:flex lg:start-0 lg:z-50 w-auto lg:right-[unset]",
+            "lg:w-[280px]": !@is_collapsed
+          }
+        >
+          <div
+            :on-click={@toggle_is_collapsed}
+            class="rounded-full shadow-md -right-3 top-6 bg-inherit w-6 h-6 cursor-pointer sidebar-panel-chevron hidden z-[10001]"
+          >
             <div class="grid place-content-center	w-6 h-6">
               <Moon.Icon name="controls-chevron-left" class={"rotate-180": @is_collapsed} />
             </div>
