683c86a9f275b8d99e27bf9ced021768597ec2ae
Margus Pärt <53759446+mxrguspxrtwork@users.noreply.github.com>
2023-07-25 15:43:48 +0300

    fix: tooltip.trigger fix

--- lib/moon/design/tooltip/trigger.ex
+++ lib/moon/design/tooltip/trigger.ex
@@ -2,4 +2,20 @@ defmodule Moon.Design.Tooltip.Trigger do
   @moduledoc false
 
   use Surface.Component, slot: "trigger"
+  import Moon.Helpers.MergeClass
+
+  prop class, :css_class
+  slot default
+
+  def render(assigns) do
+    ~F"""
+    <div aria-describedby="tooltip" class={merge([get_class(), @class])}>
+      <#slot />
+    </div>
+    """
+  end
+
+  def get_class() do
+    "group-focus:ring-2 group-focus:outline-none rounded-moon-s-sm"
+  end
 end
