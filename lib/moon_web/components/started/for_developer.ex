8602f08771b72eb1eb5e0863b13e88acd0d453e9
phgrey <phgrey@gmail.com>
2023-09-25 11:34:25 +0200

    fix: removing obsolette tailwind plugin

--- lib/moon_web/components/started/for_developer.ex
+++ lib/moon_web/components/started/for_developer.ex
@@ -233,7 +233,6 @@ defmodule MoonWeb.Components.Started.ForDeveloper do
       "postcss": "^8.4.5",
       "postcss-import": "^14.0.2",
       "tailwindcss": "^3.1.7",
-      "tailwindcss-rtl": "^0.9.0",
 
       "@popperjs/core": "^2.11.6"
     },
