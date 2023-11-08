f45140a442c546768e4b127229afa2af72bc1f8a
phgrey <phgrey@gmail.com>
2023-11-08 09:34:57 +0100

    fix: publishing library #2

---
+++ scripts/deploy/semantic-release.js
@@ -0,0 +1,34 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {name: 'beta', prerelease: true},
+    {name: 'alpha', prerelease: true},
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      },
+    ],
+    [
+      "@semantic-release/git",
+      { 
+        "assets": ["CHANGELOG.md", "VERSION"],
+        "message": "chore(release): update ${nextRelease.version} changelog & version [skip ci]"
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'echo ${nextRelease.version} > VERSION',
+        publishCmd: 'mix hex.publish --yes',
+      },
+    ],
+  ],
+};
