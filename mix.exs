d260a69bc0e902f49576fb34d1fed939a0ac3544
phgrey <phgrey@gmail.com>
2023-11-13 10:42:16 +0100

    fix: adding back web-application library

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Moon.MixProject do
     [
       licenses: ["MIT"],
       files: [
-        "lib/{moon,moon.ex}",
+        "lib",
         "config/surface.exs",
         "assets",
         "priv/static/{themes,fonts}",
