1f6de0d63a71108adfaf14552a52d9d1865a5e32
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-11-14 13:52:50 +0200

    footer fix

--- lib/moon_web/components/footer.ex
+++ lib/moon_web/components/footer.ex
@@ -9,7 +9,7 @@ defmodule MoonWeb.Components.Footer do
 
   def render(assigns) do
     ~F"""
-    <footer class="theme-moon-dark relative lg:fixed -mx-5 lg:mx-0 mt-16 lg:mt-0  lg:bottom-0 inset-x-0 flex lg:flex-row gap-x-8 gap-y-6 2xl:ps-80 flex-wrap items-center text-moon-14 text-bulma bg-goku justify-center md:justify-between">
+    <footer class="theme-moon-dark relative lg:fixed -mx-5 lg:mx-0 mt-16 lg:mt-0 z-50 lg:z-[9999] lg:bottom-0 inset-x-0 flex lg:flex-row gap-x-8 gap-y-6 2xl:ps-80 flex-wrap items-center text-moon-14 text-bulma bg-goku justify-center md:justify-between">
       <div class="w-full py-5 px-5 lg:ps-8 lg:pe-5 xl:pe-20 2xl:px-32">
         <div class="flex 2xl:max-w-screen-xl gap-x-4 gap-y-6 flex-col lg:flex-row w-full justify-between items-center">
           <Version />
