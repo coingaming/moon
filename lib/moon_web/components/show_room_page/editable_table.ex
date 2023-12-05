4d60f933a3ad13f0d61556c06eb6fa1c0a2eef69
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-12-05 10:49:04 +0200

    error fix

--- lib/moon_web/components/show_room_page/editable_table.ex
+++ lib/moon_web/components/show_room_page/editable_table.ex
@@ -22,72 +22,74 @@ defmodule MoonWeb.Components.ShowRoomPage.EditableTable do
 
   def render(assigns) do
     ~F"""
-    <Table
-      items={editable_menu_item <- @editable_menu_items}
-      {=@id}
-      class="bg-goku !border-collapse"
-      header_row_class="text-left first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus"
-      row_size="xl"
-      selected_bg="bg-heles"
-    >
-      <Column
-        name="id"
-        label="Icon"
-        class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
-        width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
+    <div>
+      <Table
+        items={editable_menu_item <- @editable_menu_items}
+        {=@id}
+        class="bg-goku !border-collapse"
+        header_row_class="text-left first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus"
+        row_size="xl"
+        selected_bg="bg-heles"
       >
-        <Dropdown
-          id={"menu-icons-#{editable_menu_item.icon}"}
-          side_values={num: editable_menu_item.id, field: :icon}
-          on_change={@change_name}
+        <Column
+          name="id"
+          label="Icon"
+          class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
+          width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
         >
-          <Dropdown.Trigger :let={value: value}>
-            {value || "Choose icon..."}
-          </Dropdown.Trigger>
-          <Dropdown.Options class="w-[26.75rem]" position="bottom">
-            <Dropdown.Header>Filters</Dropdown.Header>
-            {#for editable_menu_item <- @editable_menu_items}
-              <Dropdown.CustomOption>
-                <Icon name={editable_menu_item.icon} class="text-moon-24" />{editable_menu_item.icon}
-              </Dropdown.CustomOption>
-            {/for}
-          </Dropdown.Options>
-        </Dropdown>
-      </Column>
-      <Column
-        name="name"
-        label="Name"
-        class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
-        width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
-      >
-        <Table.Input
-          value={editable_menu_item.name}
-          side_values={num: editable_menu_item.id, field: :name}
-          on_keyup={@change_name}
-        />
-      </Column>
-      <Column
-        name="created_at"
-        label="Link"
-        class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
-        width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
-      >
-        <Table.Input
-          value={editable_menu_item.link}
-          side_values={num: editable_menu_item.id, field: :link}
-          on_keyup={@change_name}
-        />
-      </Column>
-      <Column
-        name="created_at"
-        label="Status"
-        class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
-        width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
-      >
-        <Status active />
-      </Column>
-    </Table>
-    <Button.White class="w-fit ms-2 mt-2" on_click={@add_row}>+ Add menu item</Button.White>
+          <Dropdown
+            id={"menu-icons-#{editable_menu_item.icon}"}
+            side_values={num: editable_menu_item.id, field: :icon}
+            on_change={@change_name}
+          >
+            <Dropdown.Trigger :let={value: value}>
+              {value || "Choose icon..."}
+            </Dropdown.Trigger>
+            <Dropdown.Options class="w-[26.75rem]" position="bottom">
+              <Dropdown.Header>Filters</Dropdown.Header>
+              {#for editable_menu_item <- @editable_menu_items}
+                <Dropdown.CustomOption>
+                  <Icon name={editable_menu_item.icon} class="text-moon-24" />{editable_menu_item.icon}
+                </Dropdown.CustomOption>
+              {/for}
+            </Dropdown.Options>
+          </Dropdown>
+        </Column>
+        <Column
+          name="name"
+          label="Name"
+          class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
+          width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
+        >
+          <Table.Input
+            value={editable_menu_item.name}
+            side_values={num: editable_menu_item.id, field: :name}
+            on_keyup={@change_name}
+          />
+        </Column>
+        <Column
+          name="created_at"
+          label="Link"
+          class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
+          width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
+        >
+          <Table.Input
+            value={editable_menu_item.link}
+            side_values={num: editable_menu_item.id, field: :link}
+            on_keyup={@change_name}
+          />
+        </Column>
+        <Column
+          name="created_at"
+          label="Status"
+          class="text-moon-18 text-bulma py-4 px-4 first:rounded-l-moon-s-none last:rounded-r-moon-s-none border-y-[0.5px] border-beerus font-dm-sans"
+          width="text-moon-16 text-trunks py-4 px-4 font-normal h-16"
+        >
+          <Status active />
+        </Column>
+      </Table>
+      <Button.White class="w-fit ms-2 mt-2" on_click={@add_row}>+ Add menu item</Button.White>
+    </div>
     """
   end
 end
