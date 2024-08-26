bcf5be81d7a62c5d78040e402dc6ca045d93105e
Salvatore Criscione <csalvatore@paranoici.org>
2024-08-26 15:50:32 +0300

    Revert "add console.logs (#850)"
    This reverts commit 755fdcaf1c6a3be9aab4f696dc3abb4aaa594413.

--- assets/js/hooks/auth_code.js
+++ assets/js/hooks/auth_code.js
@@ -17,44 +17,25 @@ export default {
     handleOnChange() {
         this.inputs.forEach((input, index) => {
             input.addEventListener("input", (event) => {
-                console.log(`Input event triggered on input index: ${index}`);
-
                 const value = event.target.value;
-                console.log(`Current input value: "${value}"`);
-
                 const nextInput = this.inputs[index + 1];
-                console.log(`Next input index: ${index + 1}`);
-
                 const pattern = input.getAttribute("pattern");
-                console.log(`Pattern for input: "${pattern}"`);
 
                 if (value.length > 1) {
-                    console.log(`Value length is greater than 1, truncating to first character`);
                     event.target.value = value.charAt(0);
                     if (nextInput !== undefined && this.hasFocusJump) {
-                        console.log(`Focusing next input index: ${index + 1}`);
                         nextInput.focus();
-                    } else {
-                        console.log(`No next input to focus or hasFocusJump is false`);
                     }
                 } else if (value.match(new RegExp(pattern, "gi"))) {
-                    console.log(`Value matches the pattern`);
                     if (nextInput !== undefined && this.hasFocusJump) {
-                        console.log(`Focusing next input index: ${index + 1}`);
                         nextInput.focus();
-                    } else {
-                        console.log(`No next input to focus or hasFocusJump is false`);
                     }
                 } else {
-                    console.log(`Value does not match the pattern, clearing input`);
                     event.target.value = "";
                 }
-
                 event.preventDefault();
-                console.log(`Default action prevented`);
 
                 this.updateHiddenField();
-                console.log(`Hidden field updated`);
             });
         });
     },
@@ -83,51 +64,41 @@ export default {
     handleOnPaste() {
         this.inputs.forEach((input, index) => {
             input.addEventListener("paste", (event) => {
-                console.log(`Paste event triggered on input index: ${index}`);
-
                 const count = this.inputs.length - 1;
                 const lastInput = this.inputs[count];
 
                 const pastedValue = event.clipboardData.getData("Text");
-                console.log(`Pasted value: "${pastedValue}"`);
-
                 const pattern = input.getAttribute("pattern");
-                console.log(`Pattern for input: "${pattern}"`);
 
                 let currentInput = index;
                 let pastedCount = 0;
 
                 for (let i = 0; i < pastedValue.length; i++) {
                     const pastedCharacter = pastedValue.charAt(i);
-                    console.log(`Processing character: "${pastedCharacter}"`);
 
                     if (
                         pastedCharacter.match(pattern) &&
                         currentInput < this.inputs.length
                     ) {
                         this.inputs[currentInput].value = pastedCharacter;
-                        console.log(`Assigned character "${pastedCharacter}" to input index: ${currentInput}`);
                         currentInput++;
                         pastedCount++;
                     }
                 }
 
                 const nextInput = this.inputs[currentInput];
+
                 if (nextInput !== undefined) {
-                    console.log(`Focusing next input index: ${currentInput}`);
                     nextInput.focus();
                 } else {
-                    console.log(`No more inputs to focus. Focusing last input index: ${count}`);
                     lastInput.focus();
                 }
 
                 this.updateHiddenField();
-                console.log("Hidden field updated");
 
                 this.hiddenField.dispatchEvent(
                     new Event("input", { bubbles: true })
                 );
-                console.log("Dispatched 'input' event on hidden field");
 
                 event.preventDefault();
             });
