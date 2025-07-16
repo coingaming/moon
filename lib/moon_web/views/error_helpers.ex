b0c265010fdb2592ff7edf23a0674f15ee5d974d
erikcardona <erik.cardona@yolo.com>
2025-07-16 15:28:14 +0300

    fix: use import instead of use to support override phoenix_html 4.0

--- lib/moon_web/views/error_helpers.ex
+++ lib/moon_web/views/error_helpers.ex
@@ -3,7 +3,7 @@ defmodule MoonWeb.ErrorHelpers do
   Conveniences for translating and building error messages.
   """
 
-  use Phoenix.HTML
+  import Phoenix.HTML
 
   @doc """
   Generates tag for inlined form input errors.
