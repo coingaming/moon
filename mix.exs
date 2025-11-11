e8edcc4c2e0d92402c1f06c80c0a172cab335db7
erikcardona <erik.cardona@yolo.com>
2025-11-11 16:22:39 +0200

    Update Phoenix and related dependencies to latest versions

--- mix.exs
+++ mix.exs
@@ -73,7 +73,7 @@ defmodule Moon.MixProject do
   defp deps do
     [
       {:phoenix, ">= 1.6.15"},
-      {:phoenix_live_view, "~> 1.0.17"},
+      {:phoenix_live_view, "~> 1.1"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_live_dashboard, ">= 0.7.0"},
